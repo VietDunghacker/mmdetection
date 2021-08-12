dbfdcb67dc7cd9207ce67737451306dcf53f7363
VietDunghacker <trungdunghoang92@gmail.com>
2021-08-12 11:32:16 +0700

    Update

--- configs/deformable_detr/config.py
+++ configs/deformable_detr/config.py
@@ -197,7 +197,7 @@ data = dict(
 	test=dict(pipeline=test_pipeline))
 
 # optimizer
-optimizer = dict(_delete_=True, type='AdamW', lr=0.0001, betas=(0.9, 0.999), weight_decay=0.05,
+optimizer = dict(type='AdamW', lr=0.0001, betas=(0.9, 0.999), weight_decay=0.05,
 	paramwise_cfg=dict(
 		custom_keys={
 			'backbone': dict(lr_mult=0.1),
@@ -208,7 +208,7 @@ optimizer = dict(_delete_=True, type='AdamW', lr=0.0001, betas=(0.9, 0.999), wei
 			'norm': dict(decay_mult=0.)}))
 optimizer_config = dict(grad_clip = None)
 # learning policy
-lr_config = dict(_delete_=True,
+lr_config = dict(
 	policy='CosineAnnealing',
 	min_lr_ratio = 0.12,
 	warmup='linear',
