3acfc6cdaa40c752acc8387e6d548611fa5f2b78
VietDunghacker <trungdunghoang92@gmail.com>
2021-09-11 21:30:14 +0700

    Update

--- configs/deformable_detr/swin_deformable_detr.py
+++ configs/deformable_detr/swin_deformable_detr.py
@@ -83,7 +83,7 @@ model = dict(
 	train_cfg=dict(
 		assigner=dict(
 			type='HungarianAssigner',
-			cls_cost=dict(type='ClassificationCost', weight=1.),
+			cls_cost=dict(type='ClassificationCost', weight=2.),
 			reg_cost=dict(type='BBoxL1Cost', weight=5.0, box_format='xywh'),
 			iou_cost=dict(type='IoUCost', iou_mode='giou', weight=2.0))),
 	test_cfg=dict(max_per_img=100))
