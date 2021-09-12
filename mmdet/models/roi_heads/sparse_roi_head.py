d2d254ecef1db8e964c65fac673b7edfb4311363
VietDunghacker <trungdunghoang92@gmail.com>
2021-09-12 18:00:50 +0700

    Update

--- mmdet/models/roi_heads/sparse_roi_head.py
+++ mmdet/models/roi_heads/sparse_roi_head.py
@@ -286,9 +286,7 @@ class SparseRoIHead(CascadeRoIHead):
 
 		for img_id in range(num_imgs):
 			cls_score_per_img = cls_score[img_id]
-			scores_per_img, topk_indices = cls_score_per_img.flatten(
-				0, 1).topk(
-					self.test_cfg.max_per_img, sorted=False)
+			scores_per_img, topk_indices = cls_score_per_img.flatten(0, 1).topk(self.test_cfg.max_per_img, sorted=False)
 			labels_per_img = topk_indices % num_classes
 			bbox_pred_per_img = proposal_list[img_id][topk_indices //
 													  num_classes]
