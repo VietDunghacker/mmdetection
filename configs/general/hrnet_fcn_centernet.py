7bbd69047e6eb24479f36fbee701ff1ebbb59c8a
VietDunghacker <trungdunghoang92@gmail.com>
2021-09-12 22:43:43 +0700

    Update

--- configs/general/hrnet_fcn_centernet.py
+++ configs/general/hrnet_fcn_centernet.py
@@ -172,7 +172,7 @@ data = dict(
 	test=dict(pipeline=test_pipeline))
 
 # optimizer
-optimizer = dict(type = 'SGD', lr=0.01, momentum = 0.937, weight_decay = 0.0005)
+optimizer = dict(type = 'SGD', lr=0.0032, momentum = 0.843, weight_decay = 0.00036)
 optimizer_config = dict(grad_clip = None)
 # learning policy
 lr_config = dict(_delete_=True,
