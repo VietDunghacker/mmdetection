cb5995fb3daab2749d932f8e37f1f4013ada5269
VietDunghacker <trungdunghoang92@gmail.com>
2021-09-12 11:52:30 +0700

    Update

--- mmdet/models/dense_heads/deformable_detr_head.py
+++ mmdet/models/dense_heads/deformable_detr_head.py
@@ -6,6 +6,7 @@ import torch.nn as nn
 import torch.nn.functional as F
 from mmcv.cnn import Linear, bias_init_with_prob, constant_init
 from mmcv.runner import force_fp32
+from mmcv.ops import batched_nms
 
 from mmdet.core import multi_apply
 from mmdet.models.utils.transformer import inverse_sigmoid
