5f0f0936d62617e39e385b68f0f1ea9057d2cb90
VietDunghacker <trungdunghoang92@gmail.com>
2021-09-12 18:44:03 +0700

    Update

--- mmdet/models/dense_heads/detr_head.py
+++ mmdet/models/dense_heads/detr_head.py
@@ -614,9 +614,9 @@ class DETRHead(AnchorFreeHead):
 			proposals = self._get_bboxes_single(cls_score, bbox_pred,
 												img_shape, scale_factor,
 												rescale)
-			#result_list.append(proposals)
-			bboxes, labels = proposals
-			result_list.append(tuple(self._bboxes_nms(bboxes, labels, self.test_cfg)))
+			result_list.append(proposals)
+			#bboxes, labels = proposals
+			#result_list.append(tuple(self._bboxes_nms(bboxes, labels, self.test_cfg)))
 		return result_list
 
 	def _get_bboxes_single(self,
@@ -654,14 +654,19 @@ class DETRHead(AnchorFreeHead):
 		max_per_img = self.test_cfg.get('max_per_img', self.num_query)
 		# exclude background
 		if self.loss_cls.use_sigmoid:
-			cls_score = cls_score.sigmoid()
-			scores, indexes = cls_score.view(-1).topk(max_per_img)
-			det_labels = indexes % self.num_classes
-			bbox_index = indexes // self.num_classes
+			scores, det_labels = F.softmax(cls_score, dim=-1).max(-1)
+
+			scores, bbox_index = scores.topk(min(max_per_img, len(scores)))
 			bbox_pred = bbox_pred[bbox_index]
+			det_labels = det_labels[bbox_index]
 		else:
-			scores, det_labels = F.softmax(cls_score, dim=-1)[..., :-1].max(-1)
-			scores, bbox_index = scores.topk(max_per_img)
+			scores, det_labels = F.softmax(cls_score, dim=-1).max(-1)
+
+			fg_index = det_labels < self.num_classes
+			scores = scores[fg_index]
+			det_labels = det_labels[fg_index]
+
+			scores, bbox_index = scores.topk(min(max_per_img, len(scores)))
 			bbox_pred = bbox_pred[bbox_index]
 			det_labels = det_labels[bbox_index]
 
