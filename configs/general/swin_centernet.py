b47295645df6a732743e4ab0f01d67d20c684c05
VietDunghacker <trungdunghoang92@gmail.com>
2021-09-13 11:17:37 +0700

    Update

--- configs/general/swin_centernet.py
+++ configs/general/swin_centernet.py
@@ -32,7 +32,7 @@ model = dict(
 		loss_wh=dict(type='L1Loss', loss_weight=1.0),
 		loss_offset=dict(type='L1Loss', loss_weight=1.0)),
 	train_cfg=None,
-	test_cfg=dict(topk=100, local_maximum_kernel=3, max_per_img=100, threshold = 0.05, nms_cfg = dict(type='soft_nms', iou_threshold=0.5, method='gaussian')))
+	test_cfg=dict(topk=100, local_maximum_kernel=3, max_per_img=32, threshold = 0.05, nms_cfg = dict(type='soft_nms', iou_threshold=0.5, method='gaussian')))
 
 # data setting
 dataset_type = 'CocoDataset'
