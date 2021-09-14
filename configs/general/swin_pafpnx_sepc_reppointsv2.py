1f525f0db0c0f0de7237bab686b00dac992267c1
VietDunghacker <trungdunghoang92@gmail.com>
2021-09-14 09:22:58 +0700

    Update

--- configs/general/swin_pafpnx_sepc_reppointsv2.py
+++ configs/general/swin_pafpnx_sepc_reppointsv2.py
@@ -98,7 +98,6 @@ data_root = '/content/data/'
 img_norm_cfg = dict(mean=[123.675, 116.28, 103.53], std=[58.395, 57.12, 57.375], to_rgb=True)
 albu_train_transforms = [
 	dict(type='ShiftScaleRotate', shift_limit=0.0625, scale_limit=0.0, rotate_limit=0, interpolation=1, p=0.5),
-	dict(type='ImageCompression', quality_lower=85, quality_upper=95, p=0.2),
 	dict(
 		type='OneOf',
 		transforms=[
