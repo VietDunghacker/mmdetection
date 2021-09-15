4929efe1408109e90d801ea1f7b358fbcb06c4ad
VietDunghacker <trungdunghoang92@gmail.com>
2021-09-15 11:33:15 +0700

    Update

--- configs/general/swin_pafpnx_reppointsv2.py
+++ configs/general/swin_pafpnx_reppointsv2.py
@@ -3,7 +3,7 @@ _base_ = [
 	'../_base_/schedules/schedule_1x.py', '../_base_/default_runtime.py'
 ]
 model = dict(
-	type='GFL',
+	type='RepPointsV2Detector',
 	backbone=dict(
 		type='SwinTransformer',
 		embed_dims=128,
@@ -20,57 +20,61 @@ model = dict(
 		out_indices=(0, 1, 2, 3),
 		with_cp=True,
 		init_cfg=dict(type='Pretrained', checkpoint='https://download.openmmlab.com/mmclassification/v0/swin-transformer/swin_base_224_b16x64_300e_imagenet_20210616_190742-93230b0d.pth')),
-	neck=[
-		dict(
-			type='PAFPNX',
-			in_channels=[128, 256, 512, 1024],
-			out_channels=256,
-			start_level=1,
-			add_extra_convs='on_output',
-			num_outs=5,
-			relu_before_extra_convs=True,
-			pafpn_conv_cfg=dict(type='DCNv2'),
-			norm_cfg=dict(type='GN', num_groups=32, requires_grad=True)),
-		dict(
-			type='SEPC',
-			in_channels=[256] * 5,
-			out_channels=256,
-			stacked_convs=3,
-			pconv_deform=True,
-			lcconv_deform=True,
-			ibn=True,  # please set imgs/gpu >= 4
-			pnorm_eval=False,
-			lcnorm_eval=False,
-			lcconv_padding=1,
-			pnorm_cfg=dict(type='GN', num_groups=32, requires_grad=True),
-			lcnorm_cfg=dict(type='GN', num_groups=32, requires_grad=True))
-	],
+	neck= dict(
+		type='PAFPNX',
+		in_channels=[128, 256, 512, 1024],
+		out_channels=256,
+		start_level=1,
+		add_extra_convs='on_input',
+		num_outs=5,
+		relu_before_extra_convs=True,
+		pafpn_conv_cfg=dict(type='DCNv2'),
+		norm_cfg=dict(type='GN', num_groups=32, requires_grad=True))
+	,
 	bbox_head=dict(
-		type='GFLHead',
+		type='RepPointsV2Head',
 		num_classes=34,
-		in_channels=1280,
-		stacked_convs=0,
-		feat_channels=1280,
-		anchor_generator=dict(
-			type='AnchorGenerator',
-			ratios=[1.0],
-			octave_base_scale=8,
-			scales_per_octave=1,
-			strides=[32]),
-		loss_cls=dict(type='QualityFocalLoss', use_sigmoid=False, beta=2.0, loss_weight=1.0),
-		loss_dfl=dict(type='DistributionFocalLoss', loss_weight=0.25),
-		use_dgqp = True,
-		loss_bbox=dict(type='CIoULoss', loss_weight=2.0)),
+		in_channels=256,
+		feat_channels=256,
+		point_feat_channels=256,
+		stacked_convs=3,
+		shared_stacked_convs=1,
+		first_kernel_size=3,
+		kernel_size=1,
+		corner_dim=64,
+		num_points=25,
+		gradient_mul=0.1,
+		point_strides=[8, 16, 32, 64, 128],
+		point_base_scale=4,
+		norm_cfg=dict(type='GN', num_groups=32, requires_grad=True),
+		loss_cls=dict(type='FocalLoss', use_sigmoid=True, gamma=2.0, alpha=0.25, loss_weight=1.0),
+		loss_bbox_init=dict(type='CIoULoss', loss_weight=1.0),
+		loss_bbox_refine=dict(type='CIoULoss', loss_weight = 2.0),
+		loss_heatmap=dict(type='GaussianFocalLoss', alpha=2.0, gamma=4.0, loss_weight=0.25),
+		loss_offset=dict(type='SmoothL1Loss', beta=1.0 / 9.0, loss_weight=1.0),
+		loss_sem=dict(type='SEPFocalLoss', gamma=2.0, alpha=0.25, loss_weight=0.1),
+		transform_method='exact_minmax'),
 	train_cfg = dict(
-		assigner=dict(type='ATSSAssigner', topk=16),
-		allowed_border=-1,
-		pos_weight=-1,
-		debug=False),
+		init=dict(
+			assigner=dict(type='PointAssignerV2', scale=4, pos_num=1),
+			allowed_border=-1,
+			pos_weight=-1,
+			debug=False),
+		heatmap=dict(
+			assigner=dict(type='PointHMAssigner', gaussian_bump=True, gaussian_iou=0.7),
+			allowed_border=-1,
+			pos_weight=-1,
+			debug=False),
+		refine=dict(
+			assigner=dict(type='ATSSAssigner', topk=9),
+			allowed_border=-1,
+			pos_weight=-1,
+			debug=False)),
 	test_cfg = dict(
 		nms_pre=1000,
 		min_bbox_size=0,
 		score_thr=0.05,
-		nms=dict(type='soft_nms', iou_threshold=0.6, method = 'gaussian'),
+		nms=dict(type='nms', iou_threshold=0.6),
 		max_per_img=100)
 	)
 
@@ -98,7 +102,7 @@ train_pipeline = [
 		crop_size=(0.9, 0.9)),
 	dict(
 		type='Resize',
-		img_scale=(800, 800),
+		img_scale=[(640, 640), (960, 960)],
 		multiscale_mode='range',
 		keep_ratio=True),
 	dict(
@@ -115,6 +119,7 @@ train_pipeline = [
 					  (16, 8), (8, 16), (16, 16), (16, 32), (32, 16), (32, 32),
 					  (32, 48), (48, 32), (48, 48)]),
 	dict(type='RandomFlip', flip_ratio=0.5),
+	dict(type='Pad', size_divisor=32),
 	dict(
 		type='Albu',
 		transforms=albu_train_transforms,
@@ -130,10 +135,10 @@ train_pipeline = [
 		},
 		update_pad_shape=False,
 		skip_img_without_anno=True),	
-	dict(type='Pad', size_divisor=800),
 	dict(type='Normalize', **img_norm_cfg),
-	dict(type='DefaultFormatBundle'),
-	dict(type='Collect', keys=['img', 'gt_bboxes', 'gt_labels']),
+	dict(type='LoadRPDV2Annotations', num_classes=34),
+	dict(type='RPDV2FormatBundle'),
+	dict(type='Collect', keys=['img', 'gt_bboxes', 'gt_labels', 'gt_sem_map', 'gt_sem_weights']),
 ]
 
 test_pipeline = [
