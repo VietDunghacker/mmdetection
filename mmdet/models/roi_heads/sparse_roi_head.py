4d88cb219236f9b14914bdd1307372c9aeb44196
VietDunghacker <trungdunghoang92@gmail.com>
2021-09-20 20:14:54 +0700

    Update

--- mmdet/models/roi_heads/sparse_roi_head.py
+++ mmdet/models/roi_heads/sparse_roi_head.py
@@ -281,16 +281,18 @@ class SparseRoIHead(CascadeRoIHead):
 
 		for img_id in range(num_imgs):
 			cls_score_per_img = cls_score[img_id]
+			bbox_pred_per_img = proposal_list[img_id]
 
 			if not self.bbox_head[-1].loss_cls.use_sigmoid:
 				keep_inds = cls_score_per_img.max(-1)[1] < num_classes
 				cls_score_per_img = cls_score_per_img[keep_inds][..., :-1]
+				bbox_pred_per_img = bbox_pred_per_img[keep_inds]
 
 			print(cls_score_per_img.shape)
 
 			scores_per_img, topk_indices = cls_score_per_img.flatten(0, 1).topk(self.test_cfg.max_per_img, sorted=False)
 			labels_per_img = topk_indices % num_classes
-			bbox_pred_per_img = proposal_list[img_id][topk_indices // num_classes]
+			bbox_pred_per_img = bbox_pred_per_img[topk_indices // num_classes]
 
 			if rescale:
 				scale_factor = img_metas[img_id]['scale_factor']
