fbc8cc6c3add7374781b67a65042a8806c0e9f40
VietDunghacker <trungdunghoang92@gmail.com>
2021-09-21 08:22:20 +0700

    Update

--- configs/general/swin_pafpnx_tood.py
+++ configs/general/swin_pafpnx_tood.py
@@ -3,7 +3,7 @@ _base_ = [
 	'../_base_/schedules/schedule_1x.py', '../_base_/default_runtime.py'
 ]
 model = dict(
-	type='GFL',
+	type='TOOD',
 	backbone=dict(
 		type='SwinTransformer',
 		embed_dims=128,
@@ -119,7 +119,7 @@ train_pipeline = [
 		crop_size=(0.9, 0.9)),
 	dict(
 		type='Resize',
-		img_scale=[(640, 640), (960, 960)],
+		img_scale=[(640, 640), (800, 800)],
 		multiscale_mode='range',
 		keep_ratio=True),
 	dict(
