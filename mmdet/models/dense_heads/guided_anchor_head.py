aef8d84ab1eca6d566be6a88e53143369fc83ab1
VietDunghacker <trungdunghoang92@gmail.com>
2021-09-21 22:40:01 +0700

    Update

--- mmdet/models/dense_heads/guided_anchor_head.py
+++ mmdet/models/dense_heads/guided_anchor_head.py
@@ -227,7 +227,6 @@ class GuidedAnchorHead(AnchorHead):
 			mask = loc_pred.sigmoid()[0] >= self.loc_filter_thr
 		else:
 			mask = None
-		print(x.shape)
 		cls_score = self.conv_cls(x, mask)
 		bbox_pred = self.conv_reg(x, mask)
 		return cls_score, bbox_pred, shape_pred, loc_pred
