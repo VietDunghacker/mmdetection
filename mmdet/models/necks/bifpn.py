52a1efdf25c5ccf2681ad0555dbd0815704effb8
VietDunghacker <trungdunghoang92@gmail.com>
2021-09-23 21:18:03 +0700

    Update

--- mmdet/models/necks/bifpn.py
+++ mmdet/models/necks/bifpn.py
@@ -22,7 +22,7 @@ class ActLayer(nn.Module):
 	def forward(self, nodes):
 		# Activation function
 		if (self.act_fn == "silu"):
-			nodes = nodes * torch.sigmoid(nodes)
+			nodes = F.silu(nodes)
 
 		# # Quantization-friendly hard swish
 		elif (self.act_fn == "swish"):
