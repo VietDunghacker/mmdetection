2f07b74228001808994d2ef9724edfc5de205c88
VietDunghacker <trungdunghoang92@gmail.com>
2021-09-23 21:03:22 +0700

    Update

--- mmdet/core/utils/__init__.py
+++ mmdet/core/utils/__init__.py
@@ -1,9 +1,8 @@
 # Copyright (c) OpenMMLab. All rights reserved.
-from .dist_utils import (DistOptimizerHook, all_reduce_dict, allreduce_grads,
-                         reduce_mean)
-from .misc import flip_tensor, mask2ndarray, multi_apply, unmap
+from .dist_utils import (DistOptimizerHook, all_reduce_dict, allreduce_grads, reduce_mean)
+from .misc import flip_tensor, mask2ndarray, multi_apply, unmap, vectorize_labels
 
 __all__ = [
-    'allreduce_grads', 'DistOptimizerHook', 'reduce_mean', 'multi_apply',
-    'unmap', 'mask2ndarray', 'flip_tensor', 'all_reduce_dict'
+	'allreduce_grads', 'DistOptimizerHook', 'reduce_mean', 'multi_apply',
+	'unmap', 'mask2ndarray', 'flip_tensor', 'all_reduce_dict', 'vectorize_labels' 
 ]
--- mmdet/core/utils/misc.py
+++ mmdet/core/utils/misc.py
@@ -9,77 +9,87 @@ from ..mask.structures import BitmapMasks, PolygonMasks
 
 
 def multi_apply(func, *args, **kwargs):
-    """Apply function to a list of arguments.
+	"""Apply function to a list of arguments.
 
-    Note:
-        This function applies the ``func`` to multiple inputs and
-        map the multiple outputs of the ``func`` into different
-        list. Each list contains the same type of outputs corresponding
-        to different inputs.
+	Note:
+		This function applies the ``func`` to multiple inputs and
+		map the multiple outputs of the ``func`` into different
+		list. Each list contains the same type of outputs corresponding
+		to different inputs.
 
-    Args:
-        func (Function): A function that will be applied to a list of
-            arguments
+	Args:
+		func (Function): A function that will be applied to a list of
+			arguments
 
-    Returns:
-        tuple(list): A tuple containing multiple list, each list contains \
-            a kind of returned results by the function
-    """
-    pfunc = partial(func, **kwargs) if kwargs else func
-    map_results = map(pfunc, *args)
-    return tuple(map(list, zip(*map_results)))
+	Returns:
+		tuple(list): A tuple containing multiple list, each list contains \
+			a kind of returned results by the function
+	"""
+	pfunc = partial(func, **kwargs) if kwargs else func
+	map_results = map(pfunc, *args)
+	return tuple(map(list, zip(*map_results)))
 
 
 def unmap(data, count, inds, fill=0):
-    """Unmap a subset of item (data) back to the original set of items (of size
-    count)"""
-    if data.dim() == 1:
-        ret = data.new_full((count, ), fill)
-        ret[inds.type(torch.bool)] = data
-    else:
-        new_size = (count, ) + data.size()[1:]
-        ret = data.new_full(new_size, fill)
-        ret[inds.type(torch.bool), :] = data
-    return ret
+	"""Unmap a subset of item (data) back to the original set of items (of size
+	count)"""
+	if data.dim() == 1:
+		ret = data.new_full((count, ), fill)
+		ret[inds.type(torch.bool)] = data
+	else:
+		new_size = (count, ) + data.size()[1:]
+		ret = data.new_full(new_size, fill)
+		ret[inds.type(torch.bool), :] = data
+	return ret
 
 
 def mask2ndarray(mask):
-    """Convert Mask to ndarray..
+	"""Convert Mask to ndarray..
 
-    Args:
-        mask (:obj:`BitmapMasks` or :obj:`PolygonMasks` or
-        torch.Tensor or np.ndarray): The mask to be converted.
+	Args:
+		mask (:obj:`BitmapMasks` or :obj:`PolygonMasks` or
+		torch.Tensor or np.ndarray): The mask to be converted.
 
-    Returns:
-        np.ndarray: Ndarray mask of shape (n, h, w) that has been converted
-    """
-    if isinstance(mask, (BitmapMasks, PolygonMasks)):
-        mask = mask.to_ndarray()
-    elif isinstance(mask, torch.Tensor):
-        mask = mask.detach().cpu().numpy()
-    elif not isinstance(mask, np.ndarray):
-        raise TypeError(f'Unsupported {type(mask)} data type')
-    return mask
+	Returns:
+		np.ndarray: Ndarray mask of shape (n, h, w) that has been converted
+	"""
+	if isinstance(mask, (BitmapMasks, PolygonMasks)):
+		mask = mask.to_ndarray()
+	elif isinstance(mask, torch.Tensor):
+		mask = mask.detach().cpu().numpy()
+	elif not isinstance(mask, np.ndarray):
+		raise TypeError(f'Unsupported {type(mask)} data type')
+	return mask
 
 
 def flip_tensor(src_tensor, flip_direction):
-    """flip tensor base on flip_direction.
+	"""flip tensor base on flip_direction.
 
-    Args:
-        src_tensor (Tensor): input feature map, shape (B, C, H, W).
-        flip_direction (str): The flipping direction. Options are
-          'horizontal', 'vertical', 'diagonal'.
+	Args:
+		src_tensor (Tensor): input feature map, shape (B, C, H, W).
+		flip_direction (str): The flipping direction. Options are
+		  'horizontal', 'vertical', 'diagonal'.
 
-    Returns:
-        out_tensor (Tensor): Flipped tensor.
-    """
-    assert src_tensor.ndim == 4
-    valid_directions = ['horizontal', 'vertical', 'diagonal']
-    assert flip_direction in valid_directions
-    if flip_direction == 'horizontal':
-        out_tensor = torch.flip(src_tensor, [3])
-    elif flip_direction == 'vertical':
-        out_tensor = torch.flip(src_tensor, [2])
-    else:
-        out_tensor = torch.flip(src_tensor, [2, 3])
-    return out_tensor
+	Returns:
+		out_tensor (Tensor): Flipped tensor.
+	"""
+	assert src_tensor.ndim == 4
+	valid_directions = ['horizontal', 'vertical', 'diagonal']
+	assert flip_direction in valid_directions
+	if flip_direction == 'horizontal':
+		out_tensor = torch.flip(src_tensor, [3])
+	elif flip_direction == 'vertical':
+		out_tensor = torch.flip(src_tensor, [2])
+	else:
+		out_tensor = torch.flip(src_tensor, [2, 3])
+	return out_tensor
+	
+def vectorize_labels(flat_labels, num_classes, label_weights = None):
+	prediction_number = flat_labels.shape[0]
+	labels = torch.zeros( [prediction_number, num_classes], device=flat_labels.device)
+	pos_labels = flat_labels < num_classes
+	labels[pos_labels, flat_labels[pos_labels]] = 1
+	if label_weights is not None:
+		ignore_labels = (label_weights == 0)
+		labels[ignore_labels, :] = -1
+	return labels.reshape(-1)
