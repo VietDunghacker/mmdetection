6a95e91fd24fba8f028e0b9fe2570b6884a1fc5d
VietDunghacker <trungdunghoang92@gmail.com>
2021-09-25 01:23:06 +0700

    Update

--- mmdet/models/backbones/__init__.py
+++ mmdet/models/backbones/__init__.py
@@ -8,6 +8,7 @@ from .efficientnet import EfficientNet
 from .hourglass import HourglassNet
 from .hrnet import HRNet
 from .mobilenet_v2 import MobileNetV2
+from .pvt import PyramidVisionTransformer, PyramidVisionTransformerV2
 from .regnet import RegNet
 from .res2net import Res2Net
 from .resnest import ResNeSt
@@ -21,5 +22,5 @@ __all__ = [
     'RegNet', 'ResNet', 'ResNetV1d', 'ResNeXt', 'SSDVGG', 'HRNet',
     'MobileNetV2', 'Res2Net', 'HourglassNet', 'DetectoRS_ResNet',
     'DetectoRS_ResNeXt', 'Darknet', 'ResNeSt', 'TridentResNet', 'CSPDarknet',
-    'SwinTransformer', 'EfficientNet', 'DLA'
+    'SwinTransformer', 'EfficientNet', 'DLA', 'PyramidVisionTransformer', 'PyramidVisionTransformerV2'
 ]
