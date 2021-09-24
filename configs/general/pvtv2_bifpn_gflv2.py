b8276ff48048bb571fa22d2a77dd3dae72a9f9db
VietDunghacker <trungdunghoang92@gmail.com>
2021-09-25 02:04:16 +0700

    Update

--- configs/general/pvtv2_bifpn_gflv2.py
+++ configs/general/pvtv2_bifpn_gflv2.py
@@ -7,10 +7,10 @@ model = dict(
 	backbone=dict(
 		type='PyramidVisionTransformerV2',
 		embed_dims=64,
-		num_layers=[3, 8, 27, 3],
+		num_layers=[3, 4, 18, 3],
 		out_indices = (1, 2, 3),
 		with_cp = True,
-		init_cfg=dict(checkpoint='https://github.com/whai362/PVT/releases/download/v2/pvt_v2_b4.pth')),
+		init_cfg=dict(checkpoint='https://github.com/whai362/PVT/releases/download/v2/pvt_v2_b3.pth')),
 	neck=dict(
 		type='BiFPN',
 		in_channels=[128, 320, 512],
