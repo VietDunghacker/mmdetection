ac7b0968888a31d2e1c91034fe34c49b0da8a231
VietDunghacker <trungdunghoang92@gmail.com>
2021-09-25 15:00:44 +0700

    Update

--- mmdet/datasets/dataset_wrappers.py
+++ mmdet/datasets/dataset_wrappers.py
@@ -11,7 +11,7 @@ from torch.utils.data.dataset import ConcatDataset as _ConcatDataset
 
 from .builder import DATASETS, PIPELINES
 from .coco import CocoDataset
-from pipeline import AutoAugment
+from pipelines import AutoAugment
 
 
 @DATASETS.register_module()
