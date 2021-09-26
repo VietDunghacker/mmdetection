fb395289f09945920cc3354ca7cd8acd56186a03
VietDunghacker <trungdunghoang92@gmail.com>
2021-09-26 18:17:07 +0700

    Update

--- mmdet/datasets/samplers/class_aware_sampler.py
+++ mmdet/datasets/samplers/class_aware_sampler.py
@@ -4,6 +4,8 @@ import numpy as np
 import random
 import torch
 
+from mmcv.utils import print_log
+
 from collections import defaultdict
 from terminaltables import AsciiTable
 from torch.utils.data import Sampler
