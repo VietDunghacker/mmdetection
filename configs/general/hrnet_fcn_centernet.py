03bb18293827e67c6c690b0d6620444bfc1d0d1c
VietDunghacker <trungdunghoang92@gmail.com>
2021-09-28 08:42:15 +0700

    Update

--- configs/general/hrnet_fcn_centernet.py
+++ configs/general/hrnet_fcn_centernet.py
@@ -109,7 +109,7 @@ train_pipeline = [
 			'gt_bboxes': 'bboxes'
 		},
 		update_pad_shape=False,
-		skip_img_without_anno=True),	
+		skip_img_without_anno=False),	
 	dict(type='Normalize', **img_norm_cfg),
 	dict(type='DefaultFormatBundle'),
 	dict(type='Collect', keys=['img', 'gt_bboxes', 'gt_labels']),
