4d1dfdb2854953004a64ae20acb2417c46473099
VietDunghacker <trungdunghoang92@gmail.com>
2021-09-28 22:47:13 +0700

    Update

--- mmdet/models/dense_heads/detr_head.py
+++ mmdet/models/dense_heads/detr_head.py
@@ -830,7 +830,7 @@ class DETRHead(AnchorFreeHead):
 
 		return det_bboxes, det_labels
 	def _bboxes_nms(self, bboxes, labels, cfg):
-		keep = bboxes[:, :4] > cfg.score_threshold
+		keep = bboxes[:, -1] > cfg.score_threshold
 		bboxes = bboxes[keep]
 		labels = labels[keep]
 
