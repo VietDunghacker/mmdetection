b90dccc24d44bbf22c6b2d0b4e252e4a2daa0693
VietDunghacker <trungdunghoang92@gmail.com>
2021-09-29 22:11:22 +0700

    Update

--- configs/general/swin_bifpn_sepc_gflv2.py
+++ configs/general/swin_bifpn_sepc_gflv2.py
@@ -102,8 +102,12 @@ train_pipeline = [
 				dict(type='Resize', img_scale=(800, 800), keep_ratio=True),
 			],
 			[
+				dict(type='Mosaic', center_ratio_range=(0.8, 1.2), img_scale=(720, 720), pad_val=0.0),
+				dict(type='Resize', img_scale=(800, 800), keep_ratio=True),
+			],
+			[
 				dict(type='RandomCrop', crop_type='relative_range', crop_size=(0.9, 0.9), allow_negative_crop = True),
-				dict(type='Resize', img_scale=[(480, 480), (800, 800)], multiscale_mode='range', keep_ratio=True),
+				dict(type='Resize', img_scale=[(640, 640), (800, 800)], multiscale_mode='range', keep_ratio=True),
 			]
 		]
 	),
