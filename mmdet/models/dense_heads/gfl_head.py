d4e6018589803541c1003e58a6d83bee44938570
VietDunghacker <trungdunghoang92@gmail.com>
2021-09-30 16:14:45 +0700

    Update

--- mmdet/models/dense_heads/gfl_head.py
+++ mmdet/models/dense_heads/gfl_head.py
@@ -491,7 +491,8 @@ class GFLHead(AnchorHead):
 
 				batch_mlvl_scores = inverse_sigmoid(batch_mlvl_scores)
 				batch_mlvl_scores /= torch.cat([self.instance_per_class, torch.tensor([1])]).to(batch_mlvl_scores.device).pow(0.1)
-				batch_mlvl_scores = torch.cat([batch_mlvl_fg_scores.sigmoid(), batch_mlvl_bg_scores], dim = -1)
+				batch_mlvl_scores = batch_mlvl_scores.sigmoid()
+				#batch_mlvl_scores = torch.cat([batch_mlvl_fg_scores.sigmoid(), batch_mlvl_bg_scores], dim = -1)
 
 		if with_nms:
 			det_results = []
