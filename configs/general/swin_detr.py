2e3bf0787dc0138b848adb69f4f301ab9964ca80
VietDunghacker <trungdunghoang92@gmail.com>
2021-09-30 09:47:31 +0700

    Update

--- configs/general/swin_detr.py
+++ configs/general/swin_detr.py
@@ -1,7 +1,7 @@
 _base_ = [
 	'../_base_/default_runtime.py'
 ]
-max_per_img = 128
+max_per_img = 300
 model = dict(
 	type='DETR',
 	backbone=dict(
@@ -58,14 +58,14 @@ model = dict(
 					operation_order=('self_attn', 'norm', 'cross_attn', 'norm', 'ffn', 'norm')),
 			)),
 		positional_encoding=dict(type='SinePositionalEncoding', num_feats=128, normalize=True),
- 	 	loss_cls=dict(type='CrossEntropyLoss', bg_cls_weight=0.1, use_sigmoid=False, loss_weight=1.0, class_weight=1.0),
+ 	 	loss_cls=dict(type='FocalLoss', use_sigmoid=True, gamma=1.0, alpha=0.25, loss_weight=2.0),
 		loss_bbox=dict(type='L1Loss', loss_weight=5.0),
 		loss_iou=dict(type='GIoULoss', loss_weight=2.0)),
 	# training and testing settings
 	train_cfg=dict(
 		assigner=dict(
 			type='HungarianAssigner',
-			cls_cost=dict(type='ClassificationCost', weight=1.),
+			cls_cost=dict(type='FocalLossCost', gamma=1.0, weight=2.),
 			reg_cost=dict(type='BBoxL1Cost', weight=5.0, box_format='xywh'),
 			iou_cost=dict(type='IoUCost', iou_mode='giou', weight=2.0))),
 	test_cfg=dict(
