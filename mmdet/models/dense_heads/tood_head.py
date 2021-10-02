967ea4279981111623e39460d8f2a1c9222420ab
VietDunghacker <trungdunghoang92@gmail.com>
2021-10-02 20:59:25 +0700

    Update

--- mmdet/models/dense_heads/tood_head.py
+++ mmdet/models/dense_heads/tood_head.py
@@ -111,7 +111,7 @@ class TOODHead(AnchorHead):
 		self.inter_convs = nn.ModuleList()
 		for i in range(self.stacked_convs):
 			if i < self.num_dcn_on_head:
-				conv_cfg = dict(type='DCNv2')
+				conv_cfg = dict(type='DCNv2', deform_groups=4)
 			else:
 				conv_cfg = self.conv_cfg
 			chn = self.in_channels if i == 0 else self.feat_channels
