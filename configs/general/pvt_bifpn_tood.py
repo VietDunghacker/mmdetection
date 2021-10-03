f3733625f56a8de7b213199cda95f3b38c4a4c01
VietDunghacker <trungdunghoang92@gmail.com>
2021-10-03 12:50:49 +0700

    Update

--- configs/general/pvt_bifpn_tood.py
+++ configs/general/pvt_bifpn_tood.py
@@ -213,12 +213,7 @@ optimizer = dict(
 	type='AdamW',
 	lr=0.0001,
 	betas=(0.9, 0.999),
-	weight_decay=0.05,
-	paramwise_cfg=dict(
-		custom_keys={
-			'absolute_pos_embed': dict(decay_mult=0.),
-			'relative_position_bias_table': dict(decay_mult=0.),
-			'norm': dict(decay_mult=0.)}))
+	weight_decay=0.0001)
 optimizer_config = dict(grad_clip=None)
 log_config = dict(interval = 10)
 # learning policy
