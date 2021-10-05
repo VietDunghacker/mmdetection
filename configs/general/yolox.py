d06344e92d478a44e3cadb08b4eb417184b53265
VietDunghacker <trungdunghoang92@gmail.com>
2021-10-05 19:16:26 +0700

    Update

--- configs/general/yolox.py
+++ configs/general/yolox.py
@@ -1,91 +1,32 @@
 _base_ = [
 	'../_base_/default_runtime.py'
 ]
-max_per_img = 256
 model = dict(
-	type='DeformableDETR',
+	type='YOLOX',
 	backbone=dict(
-		type='SwinTransformer',
-		embed_dims=128,
-		depths=[2, 2, 18, 2],
-		num_heads=[4, 8, 16, 32],
-		window_size=7,
-		mlp_ratio=4,
-		qkv_bias=True,
-		qk_scale=None,
-		drop_rate=0.,
-		attn_drop_rate=0.,
-		drop_path_rate=0.3,
-		patch_norm=True,
-		out_indices=(1, 2, 3),
-		with_cp=True,
-		init_cfg=dict(type='Pretrained', checkpoint='https://download.openmmlab.com/mmclassification/v0/swin-transformer/convert/swin_base_patch4_window7_224_22kto1k-f967f799.pth')),
+		type='CSPDarknet',
+		arch='p6',
+		out_indices=(2, 3, 4, 5),
+		deepen_factor=1.33,
+		widen_factor=1.25),
 	neck=dict(
-		type='ChannelMapper',
-		in_channels=[256, 512, 1024],
-		kernel_size=1,
-		out_channels=256,
-		act_cfg=None,
-		norm_cfg=dict(type='GN', num_groups=32),
-		num_outs=4),
-	bbox_head=dict(
-		type='DeformableDETRHead',
-		num_query=max_per_img,
-		num_classes=1,
-		in_channels=256,
-		as_two_stage=False,
-		with_box_refine=True,
-		transformer=dict(
-			type='DeformableDetrTransformer',
-			encoder=dict(
-				type='DetrTransformerEncoder',
-				num_layers=6,
-				transformerlayers=dict(
-					type='BaseTransformerLayer',
-					attn_cfgs=dict(type='MultiScaleDeformableAttention', embed_dims=256),
-					feedforward_channels=1024,
-					ffn_dropout=0.1,
-					operation_order=('self_attn', 'norm', 'ffn', 'norm'))),
-			decoder=dict(
-				type='DeformableDetrTransformerDecoder',
-				num_layers=6,
-				return_intermediate=True,
-				transformerlayers=dict(
-					type='DetrTransformerDecoderLayer',
-					attn_cfgs=[
-						dict(type='MultiheadAttention', embed_dims=256, num_heads=8, dropout=0.1),
-						dict(type='MultiScaleDeformableAttention', embed_dims=256)
-					],
-					feedforward_channels=1024,
-					ffn_dropout=0.1,
-					operation_order=('self_attn', 'norm', 'cross_attn', 'norm', 'ffn', 'norm')))),
-		positional_encoding=dict(type='SinePositionalEncoding', num_feats=128, normalize=True, offset=-0.5),
- 	 	loss_cls=dict(
- 	 	 	type='FocalLoss',
- 	 	 	use_sigmoid=True,
- 	 	 	gamma=2.0,
- 	 	 	alpha=0.25,
- 	 	 	loss_weight=2.0),
-		loss_bbox=dict(type='L1Loss', loss_weight=5.0),
-		loss_iou=dict(type='GIoULoss', loss_weight=2.0)),
-	# training and testing settings
-	train_cfg=dict(
-		assigner=dict(
-			type='HungarianAssigner',
-			cls_cost=dict(type='FocalLossCost', weight=2.),
-			reg_cost=dict(type='BBoxL1Cost', weight=5.0, box_format='xywh'),
-			iou_cost=dict(type='IoUCost', iou_mode='giou', weight=2.0))),
+		type='YOLOXPAFPN',
+		in_channels=[320, 640, 1280],
+		out_channels=320,
+		num_csp_blocks=4),
+	bbox_head=dict(type='YOLOXHead', num_classes=80, in_channels=320, feat_channels=320),
+	train_cfg=dict(assigner=dict(type='SimOTAAssigner', center_radius=2.5)),
 	test_cfg=dict(
-		max_per_img=max_per_img,
-		score_threshold = 0.05,
-		nms = dict(type='soft_nms', iou_threshold=0.6)))
+		max_per_img=100,
+		score_threshold = 0.01,
+		nms = dict(type='soft_nms', iou_threshold=0.65)))
 
 # data setting
 dataset_type = 'CocoDataset'
 data_root = 'data/coco/'
 img_norm_cfg = dict(mean=[123.675, 116.28, 103.53], std=[58.395, 57.12, 57.375], to_rgb=True)
 albu_train_transforms = [
-	dict(type='ShiftScaleRotate', shift_limit=0.0625, scale_limit=0, rotate_limit=0, interpolation=1, p=0.5, border_mode = 0),
+	dict(type='ShiftScaleRotate', shift_limit=0.0625, scale_limit=0.1, rotate_limit=0.1, interpolation=1, p=0.5, border_mode = 0),
 	dict(type='RandomBrightnessContrast', brightness_limit=0.1, contrast_limit=0.1),
 	dict(type='RGBShift', r_shift_limit=10, g_shift_limit=10, b_shift_limit=10),
 	dict(type='HueSaturationValue', hue_shift_limit=20, sat_shift_limit=30, val_shift_limit=20),
