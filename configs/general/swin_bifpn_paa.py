27cecd5676b3cddb328b13530e076a3dffec5738
VietDunghacker <trungdunghoang92@gmail.com>
2021-10-09 10:11:46 +0700

    Update

--- configs/general/swin_bifpn_paa.py
+++ configs/general/swin_bifpn_paa.py
@@ -75,7 +75,7 @@ model = dict(
 		min_bbox_size=0,
 		score_thr=0.05,
 		nms=dict(type='nms', iou_threshold=0.45),
-		max_per_img=100))
+		max_per_img=16))
 
 # data setting
 img_norm_cfg = dict(mean=[123.675, 116.28, 103.53], std=[58.395, 57.12, 57.375], to_rgb=True)
