70c8b7370744eec83533d249cf85bc793ad73235
VietDunghacker <trungdunghoang92@gmail.com>
2021-10-10 19:27:32 +0700

    Update

--- configs/general/swin_pafpnx_sparsercnn.py
+++ configs/general/swin_pafpnx_sparsercnn.py
@@ -42,7 +42,7 @@ model = dict(
 		proposal_feature_channel=384,
 		bbox_roi_extractor=dict(
 			type='SingleRoIExtractor',
-			roi_layer=dict(type='RoIAlign', output_size=9, sampling_ratio=0),
+			roi_layer=dict(type='RoIAlign', output_size=7, sampling_ratio=0),
 			out_channels=384,
 			featmap_strides=[4, 8, 16, 32]),
 		bbox_head=[
@@ -62,11 +62,11 @@ model = dict(
 					in_channels=384,
 					feat_channels=96,
 					out_channels=384,
-					input_feat_shape=9,
+					input_feat_shape=7,
 					act_cfg=dict(type='ReLU', inplace=True),
 					norm_cfg=dict(type='LN')),
 				loss_bbox=dict(type='SmoothL1Loss', beta=1.0/9.0, loss_weight=5.0),
-				loss_iou=dict(type='DIoULoss', loss_weight=2.0),
+				loss_iou=dict(type='CIoULoss', loss_weight=2.0),
 				loss_cls=dict(type='FocalLoss', use_sigmoid=True, gamma=2.0, alpha=0.25, loss_weight=2.0),
 				bbox_coder=dict(
 					type='DeltaXYWHBBoxCoder',
@@ -83,7 +83,7 @@ model = dict(
 					type='HungarianAssigner',
 					cls_cost=dict(type='FocalLossCost', weight=2.0),
 					reg_cost=dict(type='BBoxL1Cost', smooth=True, beta=1.0/9.0, weight=5.0),
-					iou_cost=dict(type='IoUCost', iou_mode='diou', weight=2.0)),
+					iou_cost=dict(type='IoUCost', iou_mode='ciou', weight=2.0)),
 				sampler=dict(type='PseudoSampler'),
 				pos_weight=1) for _ in range(num_stages)
 		]),
