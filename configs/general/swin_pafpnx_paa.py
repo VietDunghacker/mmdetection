c5653b42b4a26c2567e50c94a1f78bcccabd36ab
VietDunghacker <trungdunghoang92@gmail.com>
2021-10-13 09:59:04 +0700

    Update

--- configs/general/swin_pafpnx_paa.py
+++ configs/general/swin_pafpnx_paa.py
@@ -21,9 +21,9 @@ model = dict(
 		init_cfg=dict(type='Pretrained', checkpoint='https://download.openmmlab.com/mmclassification/v0/swin-transformer/convert/swin_base_patch4_window7_224_22kto1k-f967f799.pth')),
 	neck=dict(
 		type='PAFPNX',
-		in_channels=[128, 256, 512, 1024],
+		in_channels=[256, 512, 1024],
 		out_channels=256,
-		start_level=1,
+		start_level=0,
 		add_extra_convs='on_output',
 		num_outs=5,
 		relu_before_extra_convs=True,
