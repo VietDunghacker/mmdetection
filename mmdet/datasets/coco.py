40c141c4f29461825c1022810644ac7260a38e62
VietDunghacker <trungdunghoang92@gmail.com>
2021-10-14 08:36:10 +0700

    Update

--- mmdet/datasets/coco.py
+++ mmdet/datasets/coco.py
@@ -360,16 +360,15 @@ class CocoDataset(CustomDataset):
 		return result_files, tmp_dir
 
 	def evaluate(self,
-				 results,
-				 metric='bbox',
-				 logger=None,
-				 jsonfile_prefix=None,
-				 classwise=True,
-				 proposal_nums=(1, 10, 100, ),
-				 iou_thrs=None,
-				 metric_items=None):
+				results,
+				metric='bbox',
+				logger=None,
+				jsonfile_prefix=None,
+				classwise=False,
+				proposal_nums=(1, 10, 100),
+				iou_thrs=None,
+				area_range_type='COCO'):
 		"""Evaluation in COCO protocol.
-
 		Args:
 			results (list[list | tuple]): Testing results of the dataset.
 			metric (str | list[str]): Metrics to be evaluated. Options are
@@ -388,13 +387,8 @@ class CocoDataset(CustomDataset):
 				IoUs will also be computed. If not specified, [0.50, 0.55,
 				0.60, 0.65, 0.70, 0.75, 0.80, 0.85, 0.90, 0.95] will be used.
 				Default: None.
-			metric_items (list[str] | str, optional): Metric items that will
-				be returned. If not specified, ``['AR@100', 'AR@300',
-				'AR@1000', 'AR_s@1000', 'AR_m@1000', 'AR_l@1000' ]`` will be
-				used when ``metric=='proposal'``, ``['mAP', 'mAP_50', 'mAP_75',
-				'mAP_s', 'mAP_m', 'mAP_l']`` will be used when
-				``metric=='bbox' or metric=='segm'``.
-
+			area_range_type (str, optional): Type of area range to compute
+				scale-wise AP metrics. Default: 'COCO'.
 		Returns:
 			dict[str, float]: COCO style evaluation metric.
 		"""
@@ -406,9 +400,6 @@ class CocoDataset(CustomDataset):
 				raise KeyError(f'metric {metric} is not supported')
 		if iou_thrs is None:
 			iou_thrs = np.linspace(.5, 0.95, int(np.round((0.95 - .5) / .05)) + 1, endpoint=True)
-		if metric_items is not None:
-			if not isinstance(metric_items, list):
-				metric_items = [metric_items]
 
 		result_files, tmp_dir = self.format_results(results, jsonfile_prefix)
 
@@ -431,10 +422,27 @@ class CocoDataset(CustomDataset):
 				print_log(log_msg, logger=logger)
 				continue
 
+			iou_type = 'bbox' if metric == 'proposal' else metric
 			if metric not in result_files:
 				raise KeyError(f'{metric} is not in results')
 			try:
-				cocoDt = cocoGt.loadRes(result_files[metric])
+				predictions = mmcv.load(result_files[metric])
+				if iou_type == 'segm':
+					# Refer to https://github.com/cocodataset/cocoapi/blob/master/PythonAPI/pycocotools/coco.py#L331  # noqa
+					# When evaluating mask AP, if the results contain bbox,
+					# cocoapi will use the box area instead of the mask area
+					# for calculating the instance area. Though the overall AP
+					# is not affected, this leads to different
+					# small/medium/large mask AP results.
+					for x in predictions:
+						x.pop('bbox')
+					warnings.simplefilter('once')
+					warnings.warn(
+						'The key "bbox" is deleted for more accurate mask AP '
+						'of small/medium/large instances since v2.12.0. This '
+						'does not change the overall mAP calculation.',
+						UserWarning)
+				cocoDt = cocoGt.loadRes(predictions)
 			except IndexError:
 				print_log(
 					'The testing results of the whole dataset is empty.',
@@ -442,57 +450,34 @@ class CocoDataset(CustomDataset):
 					level=logging.ERROR)
 				break
 
-			iou_type = 'bbox' if metric == 'proposal' else metric
-			cocoEval = COCOeval(cocoGt, cocoDt, iou_type)
+			cocoEval = USBeval(
+				cocoGt, cocoDt, iou_type, area_range_type=area_range_type)
 			cocoEval.params.catIds = self.cat_ids
 			cocoEval.params.imgIds = self.img_ids
 			cocoEval.params.maxDets = list(proposal_nums)
 			cocoEval.params.iouThrs = iou_thrs
-			# mapping of cocoEval.stats
-			coco_metric_names = {
-				'mAP': 0,
-				'mAP_50': 1,
-				'mAP_75': 2,
-				'mAP_s': 3,
-				'mAP_m': 4,
-				'mAP_l': 5,
-				'AR@100': 6,
-				'AR@300': 7,
-				'AR@1000': 8,
-				'AR_s@1000': 9,
-				'AR_m@1000': 10,
-				'AR_l@1000': 11
-			}
-			if metric_items is not None:
-				for metric_item in metric_items:
-					if metric_item not in coco_metric_names:
-						raise KeyError(
-							f'metric item {metric_item} is not supported')
 
 			if metric == 'proposal':
 				cocoEval.params.useCats = 0
 				cocoEval.evaluate()
 				cocoEval.accumulate()
 				cocoEval.summarize()
-				if metric_items is None:
-					metric_items = [
-						'AR@100', 'AR@300', 'AR@1000', 'AR_s@1000',
-						'AR_m@1000', 'AR_l@1000'
-					]
-
-				for item in metric_items:
-					val = float(
-						f'{cocoEval.stats[coco_metric_names[item]]:.3f}')
-					eval_results[item] = val
+				for key, val in cocoEval.stats.items():
+					if key.startswith('mAP'):
+						key = f'{metric}_{key}'
+					eval_results[key] = float(f'{val:.3f}')
 			else:
 				cocoEval.evaluate()
 				cocoEval.accumulate()
 				cocoEval.summarize()
-				if classwise:   # Compute per-category AP
+				for key, val in cocoEval.stats.items():
+					if key.startswith('mAP'):
+						key = f'{metric}_{key}'
+					eval_results[key] = float(f'{val:.3f}')
+				if classwise:  # Compute per-category AP
 					# Compute per-category AP
 					# from https://github.com/facebookresearch/detectron2/
 					precisions = cocoEval.eval['precision']
-					aps = []
 					# precision: (iou, recall, cls, area range, max dets)
 					assert len(self.cat_ids) == precisions.shape[2]
 
@@ -507,9 +492,8 @@ class CocoDataset(CustomDataset):
 							ap = np.mean(precision)
 						else:
 							ap = float('nan')
-						aps.append(ap)
-						results_per_category.append((f'{nm["name"]}', f'{float(ap):0.4f}'))
-					eval_results['AP_per_class'] = aps
+						results_per_category.append(
+							(f'{nm["name"]}', f'{float(ap):0.3f}'))
 
 					num_columns = min(6, len(results_per_category) * 2)
 					results_flatten = list(
@@ -524,21 +508,18 @@ class CocoDataset(CustomDataset):
 					table = AsciiTable(table_data)
 					print_log('\n' + table.table, logger=logger)
 
-				if metric_items is None:
-					metric_items = [
+				try:
+					copypastes = []
+					coco_metrics = [
 						'mAP', 'mAP_50', 'mAP_75', 'mAP_s', 'mAP_m', 'mAP_l'
 					]
-
-				for metric_item in metric_items:
-					key = f'{metric}_{metric_item}'
-					val = float(
-						f'{cocoEval.stats[coco_metric_names[metric_item]]:.3f}'
-					)
-					eval_results[key] = val
-				ap = cocoEval.stats[:6]
-				eval_results[f'{metric}_mAP_copypaste'] = (
-					f'{ap[0]:.3f} {ap[1]:.3f} {ap[2]:.3f} {ap[3]:.3f} '
-					f'{ap[4]:.3f} {ap[5]:.3f}')
+					for coco_metric in coco_metrics:
+						copypastes.append(f'{cocoEval.stats[coco_metric]:.3f}')
+					mAP_copypaste = ' '.join(copypastes)
+					eval_results[f'{metric}_mAP_copypaste'] = mAP_copypaste
+				except KeyError:
+					pass
 		if tmp_dir is not None:
 			tmp_dir.cleanup()
 		return eval_results
+
