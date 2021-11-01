7c74e2d24d1304aa57183a3cda0586e7ad13c459
VietDunghacker <trungdunghoang92@gmail.com>
2021-11-01 19:56:15 +0700

    Update

--- mmdet/models/necks/dyhead.py
+++ mmdet/models/necks/dyhead.py
@@ -139,7 +139,7 @@ class DyConv(BaseModule):
 			if level > 0:
 				temp_fea.append(self.DyConv[2](x[level - 1], offset, mask))
 			if level < len(x) - 1:
-				temp_fea.append(F.upsample_bilinear(self.DyConv[0](x[level + 1], offset, mask), size=[feature.size(2), feature.size(3)]))
+				temp_fea.append(F.interpolate(self.DyConv[0](x[level + 1], offset, mask), size=[feature.size(2), feature.size(3)], mode = 'bilinear', align_corners=True))
 			attn_fea = []
 			res_fea = []
 			for fea in temp_fea:
