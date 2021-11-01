094560548a82a8edfcff438cf75342f37ed73abc
VietDunghacker <trungdunghoang92@gmail.com>
2021-11-01 16:42:33 +0700

    Update

--- mmdet/models/necks/dyhead.py
+++ mmdet/models/necks/dyhead.py
@@ -3,144 +3,180 @@ import torch.nn as nn
 import torch.nn.functional as F
 
 from mmcv.ops import ModulatedDeformConv2d
-from mmcv.runner import BaseModule, ModuleList
+from mmcv.runner import BaseModule
 
 from ..builder import NECKS
 
-class ScaleAwareAttention(nn.Module):
-	# Constructor
-	def __init__(self, size):
-		super(ScaleAwareAttention, self).__init__()
-		self.L, self.S, self.C = size
+def _make_divisible(v, divisor, min_value=None):
+	if min_value is None:
+		min_value = divisor
+	new_v = max(min_value, int(v + divisor / 2) // divisor * divisor)
 
-		self.conv = nn.Conv2d(in_channels=self.L, out_channels=self.L, kernel_size=1)
-		self.hard_sigmoid = nn.Hardsigmoid()
-		self.relu = nn.ReLU()
+	if new_v < 0.9 * v:
+		new_v += divisor
+	return new_v
 
-	def forward(self, feat):
-		B, L, S, C = feat.shape
-		assert L == self.L and S == self.S and C == self.C 
-		x = F.avg_pool2d(feat, (self.S, self.C)) #B, L
-		x = self.conv(x) #B, L
-		x = self.relu(x) #B, L
-		scale_attention = self.hard_sigmoid(x)
-		return scale_attention * feat
+class DYReLU(nn.Module):
+	def __init__(self, in_channels, out_channels, reduction=4, lambda_a=1.0, K2=True, use_bias=True, use_spatial=False, init_a=[1.0, 0.0], init_b=[0.0, 0.0]):
+		super(DYReLU, self).__init__()
+		self.out_channels = out_channels
+		self.lambda_a = lambda_a * 2
+		self.K2 = K2
+		self.avg_pool = nn.AdaptiveAvgPool2d(1)
 
-class SpatialAwareAttention(nn.Module):
-	# Constructor
-	def __init__(self, size, spatial_cfg):
-		super(SpatialAwareAttention, self).__init__()
-		self.L, self.S, self.C = size
+		self.use_bias = use_bias
+		if K2:
+			self.exp = 4 if use_bias else 2
+		else:
+			self.exp = 2 if use_bias else 1
+		self.init_a = init_a
+		self.init_b = init_b
 
-		self.kernel_size = spatial_cfg['kernel_size']
-		self.padding = spatial_cfg['padding']
-		self.stride = spatial_cfg['stride']
-		self.dilation = spatial_cfg['dilation']
-		self.K = self.kernel_size[0] * self.kernel_size[1]
-		self.groups = spatial_cfg['groups']
+		# determine squeeze
+		if reduction == 4:
+			squeeze = in_channels // reduction
+		else:
+			squeeze = _make_divisible(in_channels // reduction, 4)
 
-		# 3x3 Convolution with 3K out_channel output as described in Deform Conv2 paper
-		self.conv_offset = nn.Conv2d(in_channels=1, out_channels=3*self.K, kernel_size=self.kernel_size, stride=self.stride, padding=self.padding, dilation=self.dilation)
-		
-		self.deform_conv = ModulatedDeformConv2d(in_channels=self.L, out_channels=self.L, kernel_size=self.kernel_size, stride=self.stride, padding=self.padding, dilation=self.dilation, groups=self.groups)
-	
-	def forward(self, feat):
-		B, L, S, C = feat.shape
-		assert L == self.L and S == self.S and C == self.C 
-		out = self.conv_offset(feat[:, L // 2].unsqueeze(1))
-		assert len(out.shape) == 4
+		self.fc = nn.Sequential(
+			nn.Linear(in_channels, squeeze),
+			nn.ReLU(inplace=True),
+			nn.Linear(squeeze, out_channels * self.exp),
+			nn.Hardsigmoid(in_place=True)
+		)
+		if use_spatial:
+			self.spa = nn.Sequential(
+				nn.Conv2d(in_channels, 1, kernel_size=1),
+				nn.BatchNorm2d(1),
+			)
+		else:
+			self.spa = None
 
-		o1, o2, mask = torch.chunk(out, 3, dim=1)
-		offset = torch.cat((o1, o2), dim=1)
-		mask = torch.sigmoid(mask)
+	def forward(self, x):
+		if isinstance(x, list):
+			x_in = x[0]
+			x_out = x[1]
+		else:
+			x_in = x
+			x_out = x
 
-		spacial_output = self.deform_conv(feat, offset, mask)
-		return spacial_output
+		b, c, h, w = x_in.size()
+		y = self.avg_pool(x_in).view(b, c)
+		y = self.fc(y).view(b, self.out_channels * self.exp, 1, 1)
 
-class TaskAwareAttention(nn.Module):
-	def __init__(self, size, task_cfg):
-		super(TaskAwareAttention, self).__init__()
-		self.L, self.S, self.C = size
+		if self.exp == 4:
+			a1, b1, a2, b2 = torch.split(y, self.out_channels, dim=1)
+			a1 = (a1 - 0.5) * self.lambda_a + self.init_a[0]  # 1.0
+			a2 = (a2 - 0.5) * self.lambda_a + self.init_a[1]
 
-		channel_reduction = task_cfg['channel_reduction']
-		assert self.C % channel_reduction == 0
+			b1 = b1 - 0.5 + self.init_b[0]
+			b2 = b2 - 0.5 + self.init_b[1]
+			out = torch.max(x_out * a1 + b1, x_out * a2 + b2)
+		elif self.exp == 2:
+			if self.use_bias:  # bias but not PL
+				a1, b1 = torch.split(y, self.out_channels, dim=1)
+				a1 = (a1 - 0.5) * self.lambda_a + self.init_a[0]  # 1.0
+				b1 = b1 - 0.5 + self.init_b[0]
+				out = x_out * a1 + b1
 
-		self.fc1 = nn.Linear(self.C, self.C // channel_reduction)
-		self.fc2 = nn.Linear(self.C // channel_reduction, 4)
-		self.relu = nn.ReLU(inplace=True)
-		self.sigmoid = nn.Sigmoid()
+			else:
+				a1, a2 = torch.split(y, self.out_channels, dim=1)
+				a1 = (a1 - 0.5) * self.lambda_a + self.init_a[0]  # 1.0
+				a2 = (a2 - 0.5) * self.lambda_a + self.init_a[1]
+				out = torch.max(x_out * a1, x_out * a2)
+		elif self.exp == 1:
+			a1 = y
+			a1 = (a1 - 0.5) * self.lambda_a + self.init_a[0]  # 1.0
+			out = x_out * a1
 
-		self.lambdas = task_cfg.get('lambdas', torch.tensor([1.0, 1.0, 0.5, 0.5], dtype=torch.float))
-		self.init_values = task_cfg.get('init_values', torch.tensor([1.0, 1.0, 0.5, 0.5], dtype=torch.float))
-	
-	def forward(self, feat):
-		B, L, S, C = feat.shape
-		assert L == self.L and S == self.S and C == self.C 
-		feat = feat.permute(0, 3, 1, 2).contiguous() #B x C x L x S
-		x = F.avg_pool2d(feat, (self.L, self.S)).squeeze() #B x C
-		x = self.relu(self.fc1(x)) #B x (C / channel_reduction)
-		x = self.fc2(x) #B x 4
-		x = 2 * x.sigmoid() - 1 #B x 4
+		if self.spa:
+			ys = self.spa(x_in).view(b, -1)
+			ys = F.softmax(ys, dim=1).view(b, 1, h, w) * h * w
+			ys = F.hardtanh(ys, 0, 3, inplace=True)/3
+			out = out * ys
 
-		thetas = self.init_values.to(feat.device) + self.lambdas.to(feat.device) * x #B x 4
-		alphas = thetas[:, :2] #B x 2
-		betas = thetas[:, 2:] #B x 2
+		return out
 
-		feat = feat.permute(1, 2, 3, 0) #C x L x S x B
+class DyConv(nn.Module):
+	def __init__(self, in_channels=256, out_channels=256):
+		super(DyConv, self).__init__()
 
-		output = torch.maximum((alphas[:, 0] * feat + betas[:, 0]), (alphas[:, 1] * feat + betas[:, 1]))
-		output = output.permute(3, 1, 2, 0).contiguous() #B x L x S x C
-		return output
+		self.DyConv = nn.ModuleList()
+		self.DyConv.append(
+			nn.Sequential(
+				ModulatedDeformConv2d(in_channels, out_channels, kernel_size=3, stride=1),
+				nn.GroupNorm(out_channels, num_channels=16)
+			)
+		)
+		self.DyConv.append(
+			nn.Sequential(
+				ModulatedDeformConv2d(in_channels, out_channels, kernel_size=3, stride=1),
+				nn.GroupNorm(out_channels, num_channels=16)
+			)
+		)
+		self.DyConv.append(
+			nn.Sequential(
+				ModulatedDeformConv2d(in_channels, out_channels, kernel_size=3, stride=2),
+				nn.GroupNorm(out_channels, num_channels=16)
+			)
+		)
 
-class DyHeadBlock(nn.Module):
-	def __init__(self, size, spatial_cfg, task_cfg):
-		super(DyHeadBlock, self).__init__()
-		L, S, C = size
-		assert L % 2 == 1
-		block = []
-		block.append(ScaleAwareAttention(size))
-		block.append(SpatialAwareAttention(size, spatial_cfg))
-		block.append(TaskAwareAttention(size, task_cfg))
-		self.blocks = nn.Sequential(*block)
+		self.AttnConv = nn.Sequential(
+			nn.AdaptiveAvgPool2d(1),
+			nn.Conv2d(in_channels, 1, kernel_size=1),
+			nn.ReLU(inplace=True))
 
-	def forward(self, feat):
-		return self.blocks(feat)
+		self.relu = DYReLU(in_channels, out_channels)
+		self.offset = nn.Conv2d(in_channels, 27, kernel_size=3, stride=1, padding=1)
+
+	def forward(self, x):
+		next_x = []
+		for level, feature in enumerate(x):
+			offset_mask = self.offset(feature)
+			offset = offset_mask[:, :18, :, :]
+			mask = offset_mask[:, 18:, :, :].sigmoid()
+
+			temp_fea = [self.DyConv[1](feature, offset=offset, mask=mask)]
+			if level > 0:
+				temp_fea.append(self.DyConv[2](x[feature_names[level - 1]], offset=offset, mask=mask))
+			if level < len(x) - 1:
+				temp_fea.append(F.upsample_bilinear(self.DyConv[0](x[feature_names[level + 1]], offset=offset, mask=mask), size=[feature.size(2), feature.size(3)]))
+			attn_fea = []
+			res_fea = []
+			for fea in temp_fea:
+				res_fea.append(fea)
+				attn_fea.append(self.AttnConv(fea))
+
+			res_fea = torch.stack(res_fea)
+			spa_pyr_attn = F.hardsigmoid(torch.stack(attn_fea), inplace=True)
+			mean_fea = torch.mean(res_fea * spa_pyr_attn, dim=0, keepdim=False)
+			next_x.append(self.relu(mean_fea))
+
+		return next_x
 
 @NECKS.register_module()
 class DyHead(BaseModule):
 	def __init__(self,
-				 output_shape,
-				 num_stacks=6,
-				 spatial_cfg = dict(kernel_size=(3, 3), padding=1, stride=1, dilation=1, groups=1),
-				 task_cfg = dict(channel_reduction = 8),
-				 init_cfg=dict(type='Xavier', layer=['Conv2d', 'Linear'], distribution='uniform')):
-		L, S, C = output_shape
-		assert L % 2 == 1
-		super(DyHead, self).__init__(init_cfg)
-		self.output_shape = output_shape
+				 in_channels,
+				 out_channels,
+				 num_convs,
+				 conv_cfg=None,
+				 norm_cfg=None,
+				 act_cfg=None,
+				 init_cfg=dict(type='Normal', layer=['Conv2d', 'ModulatedDeformConv2d'], mean=0, std=0.01)):
+		super(DyHead, self).__init__(init_cfg=init_cfg)
+		self.in_channels = in_channels
+		self.out_channels = out_channels
 
-		blocks = []
-		for _ in range(num_stacks):
-			blocks.append(DyHeadBlock(output_shape, spatial_cfg, task_cfg))
-		self.blocks	= nn.Sequential(*blocks)
+		self.dyhead_tower = []
+		for i in range(num_convs):
+			self.dyhead_tower.append(
+				DyConv(
+					in_channels if i == 0 else out_channels,
+					out_channels,
+				)
+			)
 
-	def forward(self, feats):
-		B, C, H, W = feats[(len(feats) - 1) // 2].shape
-		assert C == self.output_shape[2] and H * W == self.output_shape[1]
-		L, S, C = self.output_shape
-		median_level = (L - 1) // 2
-
-		outputs = []
-		for i, feat in enumerate(feats):
-			if i < median_level:
-				outputs.append(F.avg_pool2d(feat, kernel_size = 2 ** (median_level - i), stride = 2 ** (median_level - i)))
-			elif i == median_level:
-				outputs.append(feat)
-			else:
-				outputs.append(F.interpolate(feat, size = (H, W), mode = 'bilinear', align_corners = False))
-		output = torch.stack(outputs, dim = 1) #B x L x C x H x W
-		output = output.view(B, L, C, H * W).permute(0, 1, 3, 2).contiguous() #B x L x S x C
-		assert (B, L, S, C) == tuple(output.shape)
-		output = self.blocks(output).permute(0, 1, 3, 2).contiguous() #B x L x C x S
-		output = output.view(B, L * C, H, W) #B x (L x C) x H x W
-		return (output, )
+	def forward(self, x):
+		dyhead_tower = self.dyhead_tower(x)
+		return dyhead_tower
