b6aed3f604214d79dcef4fc6c5b5d4e0014fd02c
VietDunghacker <trungdunghoang92@gmail.com>
2021-11-21 03:05:17 +0700

    Update

--- configs/general/swin_pafpnx_cascaderpn_cascadercnn_sabl.py
+++ configs/general/swin_pafpnx_cascaderpn_cascadercnn_sabl.py
@@ -30,38 +30,47 @@ model = dict(
 		pafpn_conv_cfg=dict(type='DCNv2'),
 		norm_cfg=dict(type='GN', num_groups=32, requires_grad=True)),
 	rpn_head=dict(
-		type='GARPNHead',
-		in_channels=256,
-		feat_channels=256,
-		approx_anchor_generator=dict(
-			type='AnchorGenerator',
-			octave_base_scale=8,
-			scales_per_octave=3,
-			ratios=[0.5, 1.0, 2.0],
-			strides=[4, 8, 16, 32, 64]),
-		square_anchor_generator=dict(
-			type='AnchorGenerator',
-			ratios=[1.0],
-			scales=[8],
-			strides=[4, 8, 16, 32, 64]),
-		anchor_coder=dict(
-			type='DeltaXYWHBBoxCoder',
-			target_means=[.0, .0, .0, .0],
-			target_stds=[0.07, 0.07, 0.14, 0.14]),
-		bbox_coder=dict(
-			type='DeltaXYWHBBoxCoder',
-			target_means=[.0, .0, .0, .0],
-			target_stds=[0.07, 0.07, 0.11, 0.11]),
-		loc_filter_thr=0.01,
-		loss_loc=dict(
-			type='FocalLoss',
-			use_sigmoid=True,
-			gamma=2.0,
-			alpha=0.25,
-			loss_weight=1.0),
-		loss_shape=dict(type='BoundedIoULoss', beta=0.2, loss_weight=1.0),
-		loss_cls=dict(type='CrossEntropyLoss', use_sigmoid=True, loss_weight=1.0),
-		loss_bbox=dict(type='SmoothL1Loss', beta=1.0, loss_weight=1.0)),
+		type='CascadeRPNHead',
+		num_stages=2,
+		stages=[
+			dict(
+				type='StageCascadeRPNHead',
+				in_channels=256,
+				feat_channels=256,
+				anchor_generator=dict(
+					type='AnchorGenerator',
+					scales=[8],
+					ratios=[1.0],
+					strides=[4, 8, 16, 32, 64]),
+				adapt_cfg=dict(type='dilation', dilation=3),
+				bridged_feature=True,
+				sampling=False,
+				with_cls=False,
+				reg_decoded_bbox=True,
+				bbox_coder=dict(
+					type='DeltaXYWHBBoxCoder',
+					target_means=(.0, .0, .0, .0),
+					target_stds=(0.1, 0.1, 0.5, 0.5)),
+				loss_bbox=dict(type='CIoULoss', loss_weight=7.0)),
+			dict(
+				type='StageCascadeRPNHead',
+				in_channels=256,
+				feat_channels=256,
+				adapt_cfg=dict(type='offset'),
+				bridged_feature=False,
+				sampling=True,
+				with_cls=True,
+				reg_decoded_bbox=True,
+				bbox_coder=dict(
+					type='DeltaXYWHBBoxCoder',
+					target_means=(.0, .0, .0, .0),
+					target_stds=(0.05, 0.05, 0.1, 0.1)),
+				loss_cls=dict(
+					type='CrossEntropyLoss',
+					use_sigmoid=True,
+					loss_weight=0.7),
+				loss_bbox=dict(type='CIoULoss', loss_weight=7.0))
+		]),
 	roi_head=dict(
 		type='CascadeRoIHead',
 		num_stages=3,
@@ -74,7 +83,7 @@ model = dict(
 		bbox_head=[
 			dict(
 				type='SABLHead',
-				num_classes=37,
+				num_classes=40,
 				cls_in_channels=256,
 				reg_in_channels=256,
 				roi_feat_size=7,
@@ -92,12 +101,12 @@ model = dict(
 				norm_cfg=None,
 				bbox_coder=dict(type='BucketingBBoxCoder', num_buckets=14, scale_factor=1.7),
 				cls_predictor_cfg=dict(type='NormedLinear', tempearture=20),
-				loss_cls=dict(type='SeesawLoss', p=0.8, q=2.0, num_classes=37, loss_weight=1.0),
+				loss_cls=dict(type='SeesawLoss', p=0.8, q=2.0, num_classes=40, loss_weight=1.0),
 				loss_bbox_cls=dict(type='CrossEntropyLoss', use_sigmoid=True, loss_weight=1.0),
 				loss_bbox_reg=dict(type='SmoothL1Loss', beta=0.1, loss_weight=1.0)),
 			dict(
 				type='SABLHead',
-				num_classes=37,
+				num_classes=40,
 				cls_in_channels=256,
 				reg_in_channels=256,
 				roi_feat_size=7,
@@ -115,12 +124,12 @@ model = dict(
 				norm_cfg=None,
 				bbox_coder=dict(type='BucketingBBoxCoder', num_buckets=14, scale_factor=1.5),
 				cls_predictor_cfg=dict(type='NormedLinear', tempearture=20),
-				loss_cls=dict(type='SeesawLoss', p=0.8, q=2.0, num_classes=37, loss_weight=1.0),
+				loss_cls=dict(type='SeesawLoss', p=0.8, q=2.0, num_classes=40, loss_weight=1.0),
 				loss_bbox_cls=dict(type='CrossEntropyLoss', use_sigmoid=True, loss_weight=1.0),
 				loss_bbox_reg=dict(type='SmoothL1Loss', beta=0.1, loss_weight=1.0)),
 			dict(
 				type='SABLHead',
-				num_classes=37,
+				num_classes=40,
 				cls_in_channels=256,
 				reg_in_channels=256,
 				roi_feat_size=7,
@@ -138,43 +147,35 @@ model = dict(
 				norm_cfg=None,
 				bbox_coder=dict(type='BucketingBBoxCoder', num_buckets=14, scale_factor=1.3),
 				cls_predictor_cfg=dict(type='NormedLinear', tempearture=20),
-				loss_cls=dict(type='SeesawLoss', p=0.8, q=2.0, num_classes=37, loss_weight=1.0),
+				loss_cls=dict(type='SeesawLoss', p=0.8, q=2.0, num_classes=40, loss_weight=1.0),
 				loss_bbox_cls=dict(type='CrossEntropyLoss', use_sigmoid=True, loss_weight=1.0),
 				loss_bbox_reg=dict(type='SmoothL1Loss', beta=0.1, loss_weight=1.0))
 		]),
 	# model training and testing settings
 	train_cfg=dict(
-		rpn=dict(
-			assigner=dict(
-				type='MaxIoUAssigner',
-				pos_iou_thr=0.7,
-				neg_iou_thr=0.3,
-				min_pos_iou=0.3,
-				match_low_quality=True,
-				ignore_iof_thr=-1),
-			sampler=dict(
-				type='RandomSampler',
-				num=256,
-				pos_fraction=0.5,
-				neg_pos_ub=-1,
-				add_gt_as_proposals=False),
-			ga_assigner=dict(
-				type='ApproxMaxIoUAssigner',
-				pos_iou_thr=0.7,
-				neg_iou_thr=0.3,
-				min_pos_iou=0.3,
-				ignore_iof_thr=-1),
-			ga_sampler=dict(
-				type='RandomSampler',
-				num=256,
-				pos_fraction=0.5,
-				neg_pos_ub=-1,
-				add_gt_as_proposals=False),
-			allowed_border=-1,
-			center_ratio=0.2,
-			ignore_ratio=0.5,
-			pos_weight=-1,
-			debug=False),
+		rpn=[
+			dict(
+				assigner=dict(type='RegionAssigner', center_ratio=0.2, ignore_ratio=0.5),
+				allowed_border=-1,
+				pos_weight=-1,
+				debug=False),
+			dict(
+				assigner=dict(
+					type='MaxIoUAssigner',
+					pos_iou_thr=0.7,
+					neg_iou_thr=0.7,
+					min_pos_iou=0.3,
+					ignore_iof_thr=-1),
+				sampler=dict(
+					type='RandomSampler',
+					num=256,
+					pos_fraction=0.5,
+					neg_pos_ub=-1,
+					add_gt_as_proposals=False),
+				allowed_border=-1,
+				pos_weight=-1,
+				debug=False)
+		],
 		rpn_proposal=dict(
 			nms_pre=2000,
 			nms_post=1000,
@@ -245,7 +246,7 @@ model = dict(
 	)
 
 # data setting
-img_norm_cfg = dict(mean=[123.675, 116.28, 103.53], std=[58.395, 57.12, 57.375], to_rgb=True)
+img_norm_cfg = dict(mean=[123.675, 116.28, 103.53], std=[58.395, 57.12, 57.405], to_rgb=True)
 albu_train_transforms = [
 	dict(type='ShiftScaleRotate', shift_limit=0.0625, scale_limit=0.0625, rotate_limit=1, interpolation=1, p=0.5, border_mode = 0),
 	dict(type='RandomBrightnessContrast', brightness_limit=0.1, contrast_limit=0.1),
@@ -266,11 +267,11 @@ train_pipeline = [
 		policies = [
 			[
 				dict(type='Mosaic', center_ratio_range=(0.9, 1.1), img_scale=(960, 960), pad_val=0.0),
-				dict(type='Resize', img_scale=[(370, 370), (960, 960)], multiscale_mode='range', keep_ratio=True),
+				dict(type='Resize', img_scale=[(400, 400), (960, 960)], multiscale_mode='range', keep_ratio=True),
 			],
 			[
 				dict(type='Mosaic', center_ratio_range=(0.8, 1.2), img_scale=(960, 960), pad_val=0.0),
-				dict(type='Resize', img_scale=[(370, 370), (960, 960)], multiscale_mode='range', keep_ratio=True),
+				dict(type='Resize', img_scale=[(400, 400), (960, 960)], multiscale_mode='range', keep_ratio=True),
 			],
 			[
 				dict(type='Resize', img_scale=(960, 960), keep_ratio=True),
@@ -303,7 +304,7 @@ train_pipeline = [
 						dict(
 							type = "OneOf",
 							transforms=[
-								dict(type = "Crop", x_min = 0, y_min = i, x_max = 960, y_max = 960) for i in range(437, 720, 10)
+								dict(type = "Crop", x_min = 0, y_min = i, x_max = 960, y_max = 960) for i in range(440, 720, 10)
 								],
 							p=1.0),
 						dict(type='ShiftScaleRotate', shift_limit=0.0625, scale_limit=0.1, rotate_limit=45, interpolation=1, p=0.5, border_mode = 0),					
