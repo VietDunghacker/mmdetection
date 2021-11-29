1d615f41204eec1c9153d7f0af4409f70f332762
VietDunghacker <trungdunghoang92@gmail.com>
2021-11-29 09:40:28 +0700

    Update

--- configs/general/swin_cornernet.py
+++ configs/general/swin_cornernet.py
@@ -200,7 +200,17 @@ data = dict(
 	test=dict(pipeline=test_pipeline))
 
 # optimizer
-optimizer = dict(_delete_ = True, type = 'AdamW', lr=0.0005, weight_decay = 0.0001)
+optimizer = dict(
+	_delete_ = True,
+	type='AdamW',
+	lr=0.0001,
+	betas=(0.9, 0.999),
+	weight_decay=0.05,
+	paramwise_cfg=dict(
+		custom_keys={
+			'absolute_pos_embed': dict(decay_mult=0.),
+			'relative_position_bias_table': dict(decay_mult=0.),
+			'norm': dict(decay_mult=0.)}))
 optimizer_config = dict(grad_clip = None)
 # learning policy
 lr_config = dict(_delete_=True,
