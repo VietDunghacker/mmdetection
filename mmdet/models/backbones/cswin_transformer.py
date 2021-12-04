79cd233f5e86ce85f9d7e31207e1f86e8aa34d52
VietDunghacker <trungdunghoang92@gmail.com>
2021-12-05 01:02:16 +0700

    Update

--- mmdet/models/backbones/cswin_transformer.py
+++ mmdet/models/backbones/cswin_transformer.py
@@ -403,7 +403,7 @@ class CSWin(BaseModule):
 			for blk in blocks:
 				blk.H = H
 				blk.W = W
-				if self.with_cp:
+				if self.with_cp and x.requires_grad:
 					x = checkpoint.checkpoint(blk, x)
 				else:
 					x = blk(x)
