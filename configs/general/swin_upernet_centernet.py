cfbd3081ba8e8da19745ab3d28425ae7b2ab4e03
VietDunghacker <trungdunghoang92@gmail.com>
2021-12-20 19:20:22 +0700

    Update

--- configs/general/swin_upernet_centernet.py
+++ configs/general/swin_upernet_centernet.py
@@ -70,15 +70,15 @@ train_pipeline = [
 		policies = [
 			[
 				dict(type='Mosaic', center_ratio_range=(0.8, 1.2), img_scale=(640, 640), pad_val=0.0),
-				dict(type='Resize', img_scale=[(640, 640), (960, 960)], multiscale_mode='range', keep_ratio=True),
-			],
-			[
-				dict(type='RandomCrop', crop_type='relative_range', crop_size=(0.9, 0.9), allow_negative_crop = True),
-				dict(type='Resize', img_scale=[(640, 640), (960, 960)], multiscale_mode='range', keep_ratio=True),
+				dict(type='Resize', img_scale=(960, 960), keep_ratio=True),
 			],
 			[
 				dict(type='RandomCrop', crop_type='relative_range', crop_size=(0.8, 0.8), allow_negative_crop = True),
-				dict(type='Resize', img_scale=[(640, 640), (960, 960)], multiscale_mode='range', keep_ratio=True),
+				dict(type='Resize', img_scale=(960, 960), keep_ratio=True),
+			],
+			[
+				dict(type='RandomCrop', crop_type='relative_range', crop_size=(0.9, 0.9), allow_negative_crop = True),
+				dict(type='Resize', img_scale=(960, 960), keep_ratio=True),
 			]
 		]
 	),
