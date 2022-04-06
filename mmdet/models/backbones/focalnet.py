f108adc3681f68a31b5f968172c7deb9b812532a
VietDunghacker <trungdunghoang92@gmail.com>
2022-04-06 19:48:18 +0900

    Update

--- mmdet/models/backbones/focalnet.py
+++ mmdet/models/backbones/focalnet.py
@@ -418,7 +418,7 @@ class FocalNet(BaseModule):
 
 		def _init_weights(m):
 			if isinstance(m, nn.Linear):
-				trunc_normal_(m.weight, std=.02)
+				trunc_normal_init(m.weight, std=.02)
 				if isinstance(m, nn.Linear) and m.bias is not None:
 					nn.init.constant_(m.bias, 0)
 			elif isinstance(m, nn.LayerNorm):
