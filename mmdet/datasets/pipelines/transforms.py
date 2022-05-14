0febe3b9ac145020fce411b72a5965a79e175b32
VietDunghacker <trungdunghoang92@gmail.com>
2022-05-14 15:44:20 +0900

    Update

--- mmdet/datasets/pipelines/transforms.py
+++ mmdet/datasets/pipelines/transforms.py
@@ -2679,7 +2679,12 @@ class RandomMaskFace:
 
 					x1, y1, x2, y2 = int(x1), int(y1), int(x2), int(y2)
 
-					img[y1 : y2, x1 : x2] = img[y1 : y2, x1 : x2] // 10
+					mask = np.random.rand(y2 - y1, x2 - x1)
+					cropped_region = img[y1 : y2, x1 : x2]
+					random_color = np.random.randint(0, 255, (y2 - y1, x2 - x1, 3))
+					cropped_region[mask] = random_color[mask]
+
+					img[y1 : y2, x1 : x2] = cropped_region
 
 					del boxes[erase_idx]
 
