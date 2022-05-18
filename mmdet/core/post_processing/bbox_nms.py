0fdf72e42308fa0cc18d9498cad449e0350ae53b
VietDunghacker <trungdunghoang92@gmail.com>
2022-05-18 17:12:09 +0900

    Update

--- mmdet/core/post_processing/bbox_nms.py
+++ mmdet/core/post_processing/bbox_nms.py
@@ -92,6 +92,7 @@ def multiclass_nms(multi_bboxes,
 	iou_thr = nms_cfg['iou_threshold']
 	num_classes = multi_scores.size(1) - 1
 	# exclude background category
+	'''
 	if multi_bboxes.shape[1] > 4:
 		bboxes = multi_bboxes.view(multi_scores.size(0), -1, 4)
 	else:
@@ -100,13 +101,13 @@ def multiclass_nms(multi_bboxes,
 	scores = multi_scores[:, :-1]
 
 	labels = torch.arange(num_classes, dtype=torch.long, device=scores.device)
-	labels = labels.view(1, -1).expand_as(scores)
+	labels = labels.view(1, -1).expand_as(scores)'''
 
-	'''scores, labels = torch.max(multi_scores, dim=-1)
+	scores, labels = torch.max(multi_scores, dim=-1)
 	if multi_bboxes.shape[1] > 4:
 		bboxes = multi_bboxes.view(multi_scores.size(0), -1, 4)[range(multi_scores.size(0)), labels]
 	else:
-		bboxes = multi_bboxes'''
+		bboxes = multi_bboxes
 
 	bboxes = bboxes.reshape(-1, 4)
 	scores = scores.reshape(-1)
