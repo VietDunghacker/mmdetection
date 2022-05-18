f0777409357b5638eea9a4802a0fb61d50fced86
VietDunghacker <trungdunghoang92@gmail.com>
2022-05-18 17:16:41 +0900

    Update

--- configs/general/swinv2_bifpn_sepc_reppointsv2.py
+++ configs/general/swinv2_bifpn_sepc_reppointsv2.py
@@ -17,7 +17,7 @@ model = dict(
 		patch_norm=True,
 		out_indices=(1, 2, 3),
 		with_cp=True,
-		pretrained='https://github.com/SwinTransformer/storage/releases/download/v2.0.0/swinv2_base_patch4_window12_192_22k.pth'),
+		pretrained='https://github.com/SwinTransformer/storage/releases/download/v2.0.0/swinv2_base_patch4_window12to16_192to256_22kto1k_ft.pth'),
 	neck=[
 		dict(
 			type='BiFPN',
