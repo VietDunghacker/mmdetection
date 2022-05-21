79b68784dafc8c0700585ddd6406787afc422cf0
VietDunghacker <trungdunghoang92@gmail.com>
2022-05-21 16:41:52 +0900

    Update

--- mmdet/datasets/usbeval.py
+++ mmdet/datasets/usbeval.py
@@ -67,8 +67,7 @@ class USBeval(COCOeval):
 		:return: None
 		"""
 		super().__init__(cocoGt=cocoGt, cocoDt=cocoDt, iouType=iouType)
-		area_ranges, area_labels, relative_area = calc_area_range_info(
-			area_range_type)
+		area_ranges, area_labels, relative_area = calc_area_range_info(area_range_type)
 		self.params.areaRng = area_ranges
 		self.params.areaRngLbl = area_labels
 		self.relative_area = relative_area
@@ -118,6 +117,7 @@ class USBeval(COCOeval):
 		dtm = np.zeros((T, D))
 		gtIg = np.array([g['_ignore'] for g in gt])
 		dtIg = np.zeros((T, D))
+		dtIoU = np.zeros((T, D))
 		if not len(ious) == 0:
 			for tind, t in enumerate(p.iouThrs):
 				for dind, d in enumerate(dt):
@@ -143,6 +143,7 @@ class USBeval(COCOeval):
 					dtIg[tind, dind] = gtIg[m]
 					dtm[tind, dind] = gt[m]['id']
 					gtm[tind, m] = d['id']
+					dtIoU[tind, dind] = iou
 		# set unmatched detections outside of area range to ignore
 		if self.relative_area:
 			a = np.array([
@@ -150,8 +151,7 @@ class USBeval(COCOeval):
 				or d['area'] / img_area > aRng[1] for d in dt
 			])
 		else:
-			a = np.array(
-				[d['area'] < aRng[0] or d['area'] > aRng[1] for d in dt])
+			a = np.array([d['area'] < aRng[0] or d['area'] > aRng[1] for d in dt])
 		a = a.reshape((1, len(dt)))
 		dtIg = np.logical_or(dtIg, np.logical_and(dtm == 0, np.repeat(a, T, 0)))
 		# store results for given image and category
@@ -167,7 +167,173 @@ class USBeval(COCOeval):
 			'dtScores': [d['score'] for d in dt],
 			'gtIgnore': gtIg,
 			'dtIgnore': dtIg,
+			'dtIoUs': dtIoU,
 		}
+
+	def accumulate_lrp(self, p=None):
+		'''
+		Accumulate per image evaluation results and
+		store the result in self.eval
+		:param p: input params for evaluation
+		:return: None
+		'''
+		print('Accumulating evaluation results...')
+		tic = time.time()
+		if not self.evalImgs:
+			print('Please run evaluate() first')
+		# allows input customized parameters
+		if p is None:
+			p = self.params
+		p.catIds = p.catIds if p.useCats == 1 else [-1]
+		T = len(p.iouThrs)
+		R = len(p.recThrs)
+		K = len(p.catIds) if p.useCats else 1
+		A = len(p.areaRng)
+		M = len(p.maxDets)
+		# -1 for the precision of absent categories
+		precision = -np.ones((T, R, K, A, M))
+		recall = -np.ones((T, K, A, M))
+		scores = -np.ones((T, R, K, A, M))
+		olrp_loc = -np.ones((K, A, M))
+		olrp_fp = -np.ones((K, A, M))
+		olrp_fn = -np.ones((K, A, M))
+		olrp = -np.ones((K, A, M))
+		lrp_opt_thr = -np.ones((K, A, M))
+
+		# create dictionary for future indexing
+		_pe = self._paramsEval
+		catIds = _pe.catIds if _pe.useCats else [-1]
+		setK = set(catIds)
+		setA = set(map(tuple, _pe.areaRng))
+		setM = set(_pe.maxDets)
+		setI = set(_pe.imgIds)
+		# get inds to evaluate
+		k_list = [n for n, k in enumerate(p.catIds) if k in setK]
+		m_list = [m for n, m in enumerate(p.maxDets) if m in setM]
+		a_list = [
+			n for n, a in enumerate(map(lambda x: tuple(x), p.areaRng))
+			if a in setA
+		]
+		i_list = [n for n, i in enumerate(p.imgIds) if i in setI]
+		I0 = len(_pe.imgIds)
+		A0 = len(_pe.areaRng)
+		# retrieve E at each category, area range, and max number of detections
+		for k, k0 in enumerate(k_list):
+			Nk = k0 * A0 * I0
+			for a, a0 in enumerate(a_list):
+				Na = a0 * I0
+				for m, maxDet in enumerate(m_list):
+					E = [self.evalImgs[Nk + Na + i] for i in i_list]
+					E = [e for e in E if e is not None]
+					if len(E) == 0:
+						continue
+					dtScores = np.concatenate(
+						[e['dtScores'][0:maxDet] for e in E])
+
+					# different sorting method generates slightly
+					# different results.
+					# mergesort is used to be consistent as Matlab
+					# implementation.
+					inds = np.argsort(-dtScores, kind='mergesort')
+					dtScoresSorted = dtScores[inds]
+
+					dtm = np.concatenate(
+						[e['dtMatches'][:, 0:maxDet] for e in E], axis=1)[:,
+																		  inds]
+					dtIg = np.concatenate(
+						[e['dtIgnore'][:, 0:maxDet] for e in E], axis=1)[:,
+																		 inds]
+					dtIoU = np.concatenate(
+						[e['dtIoUs'][:, 0:maxDet] for e in E], axis=1)[:, inds]
+
+					gtIg = np.concatenate([e['gtIgnore'] for e in E])
+					npig = np.count_nonzero(gtIg == 0)
+					if npig == 0:
+						continue
+					tps = np.logical_and(dtm, np.logical_not(dtIg))
+					fps = np.logical_and(np.logical_not(dtm),
+										 np.logical_not(dtIg))
+
+					dtIoU = np.multiply(dtIoU, tps)
+					tp_sum = np.cumsum(tps, axis=1).astype(dtype=np.float)
+					fp_sum = np.cumsum(fps, axis=1).astype(dtype=np.float)
+					for t, (tp, fp) in enumerate(zip(tp_sum, fp_sum)):
+						tp = np.array(tp)
+						fp = np.array(fp)
+						nd = len(tp)
+						rc = tp / npig
+						pr = tp / (fp + tp + np.spacing(1))
+						q = np.zeros((R, ))
+						ss = np.zeros((R, ))
+
+						if nd:
+							recall[t, k, a, m] = rc[-1]
+						else:
+							recall[t, k, a, m] = 0
+
+						# numpy is slow without cython optimization
+						# for accessing elements use python array
+						# gets significant speed improvement
+						pr = pr.tolist()
+						q = q.tolist()
+
+						for i in range(nd - 1, 0, -1):
+							if pr[i] > pr[i - 1]:
+								pr[i - 1] = pr[i]
+
+						inds = np.searchsorted(rc, p.recThrs, side='left')
+						try:
+							for ri, pi in enumerate(inds):
+								q[ri] = pr[pi]
+								ss[ri] = dtScoresSorted[pi]
+						except BaseException:
+							pass
+						precision[t, :, k, a, m] = np.array(q)
+						scores[t, :, k, a, m] = np.array(ss)
+
+					# oLRP and Opt.Thr. Computation
+					tp_num = np.cumsum(tps[0, :])
+					fp_num = np.cumsum(fps[0, :])
+					fn_num = npig - tp_num
+					# If there is detection
+					if tp_num.shape[0] > 0:
+						# There is some TPs
+						if tp_num[-1] > 0:
+							total_loc = tp_num - np.cumsum(dtIoU[0, :])
+							lrps = (total_loc / (1 - _pe.iouThrs[0]) + fp_num +
+									fn_num) / (tp_num + fp_num + fn_num)
+							opt_pos_idx = np.argmin(lrps)
+							olrp[k, a, m] = lrps[opt_pos_idx]
+							olrp_loc[k, a, m] = total_loc[opt_pos_idx] / \
+								tp_num[opt_pos_idx]
+							olrp_fp[k, a, m] = fp_num[opt_pos_idx] / \
+								(tp_num[opt_pos_idx] + fp_num[opt_pos_idx])
+							olrp_fn[k, a, m] = fn_num[opt_pos_idx] / npig
+							lrp_opt_thr[k, a, m] = dtScoresSorted[opt_pos_idx]
+						# There is No TP
+						else:
+							olrp_loc[k, a, m] = np.nan
+							olrp_fp[k, a, m] = np.nan
+							olrp_fn[k, a, m] = 1.
+							olrp[k, a, m] = 1.
+							lrp_opt_thr[k, a, m] = np.nan
+					# No detection
+					else:
+						olrp_loc[k, a, m] = np.nan
+						olrp_fp[k, a, m] = np.nan
+						olrp_fn[k, a, m] = 1.
+						olrp[k, a, m] = 1.
+						lrp_opt_thr[k, a, m] = np.nan
+		self.eval.update({
+			'olrp_loc': olrp_loc,
+			'olrp_fp': olrp_fp,
+			'olrp_fn': olrp_fn,
+			'olrp': olrp,
+			'lrp_opt_thr': lrp_opt_thr,
+		})
+		toc = time.time()
+		print('DONE (t={:0.2f}s).'.format(toc - tic))
+
 
 	def _summarize(self, ap=1, iouThr=None, areaRng='all', maxDets=100):
 		"""Compute and display a specific metric."""
@@ -190,13 +356,45 @@ class USBeval(COCOeval):
 					t = np.where(iouThr == p.iouThrs)[0]
 				s = s[t]
 			s = s[:, :, :, aind, mind]
-		else:
+			if len(s[s > -1]) == 0:
+				mean_s = -1
+			else:
+				mean_s = np.mean(s[s > -1])
+		elif ap == 0:
 			# dimension of recall: [TxKxAxM]
 			s = self.eval['recall']
 			if iouThr is not None:
 				t = np.where(iouThr == p.iouThrs)[0]
 				s = s[t]
 			s = s[:, :, aind, mind]
+		else:
+			# # dimension of LRP: [KxAxM]
+			# Person 0, Broccoli 50
+			if lrp_type == 'oLRP':
+				s = self.eval['olrp'][:, aind, mind]
+				titleStr = 'Optimal LRP'
+				typeStr = '	'
+			if lrp_type == 'oLRP_Localisation':
+				s = self.eval['olrp_loc'][:, aind, mind]
+				titleStr = 'Optimal LRP Loc'
+				typeStr = '	'
+			if lrp_type == 'oLRP_false_positive':
+				s = self.eval['olrp_fp'][:, aind, mind]
+				titleStr = 'Optimal LRP FP'
+				typeStr = '	'
+			if lrp_type == 'oLRP_false_negative':
+				s = self.eval['olrp_fn'][:, aind, mind]
+				titleStr = 'Optimal LRP FN'
+				typeStr = '	'
+			if lrp_type == 'oLRP_thresholds':
+				s = self.eval['lrp_opt_thr'][:, aind, mind].squeeze(axis=1)
+				titleStr = '# Class-specific LRP-Optimal Thresholds # \n'
+				typeStr = '	'
+				# Floor by using 3 decimal digits
+				print(titleStr, np.round(s - 0.5 * 10**(-3), 3))
+				return s
+		idx = (~np.isnan(s))
+		s = s[idx]
 		if len(s[s > -1]) == 0:
 			mean_s = -1
 		else:
@@ -225,8 +423,7 @@ class USBeval(COCOeval):
 				for i in range(50, 100, 5):
 					stats['mAP_{}'.format(i)] = self._summarize(1, iouThr=i/100, maxDets=max_dets[2])
 			else:
-				stats[f'mAP_{area_label_short}'] = self._summarize(
-					1, areaRng=area_label, maxDets=max_dets[2])
+				stats[f'mAP_{area_label_short}'] = self._summarize(1, areaRng=area_label, maxDets=max_dets[2])
 		# summarize AR
 		for area_label in self.params.areaRngLbl:
 			area_label_short = self._shorten_area_label(area_label)
@@ -236,6 +433,15 @@ class USBeval(COCOeval):
 			elif area_label in ['small', 'medium', 'large']:
 				key = f'AR_{area_label_short}@{max_dets[2]}'
 				stats[key] = self._summarize(0, areaRng=area_label, maxDets=max_dets[2])
+
+		stats['oLRP'] = _summarize(-1, iouThr=.5, areaRng='all', maxDets=max_dets, lrp_type='oLRP')
+		stats['oLRP_loc'] = _summarize(-1, iouThr=.5, areaRng='all', maxDets=max_dets, lrp_type='oLRP_Localisation')
+		stats['oLRP_fp'] = _summarize(-1, iouThr=.5, areaRng='all', maxDets=max_dets, lrp_type='oLRP_false_positive')
+		stats['oLRP_fn'] = _summarize(-1, iouThr=.5, areaRng='all', maxDets=max_dets, lrp_type='oLRP_false_negative')
+		stats['oLRP_small'] = _summarize(-1, iouThr=.5, areaRng='small', maxDets=max_dets, lrp_type='oLRP')
+		stats['oLRP_medium'] = _summarize(-1, iouThr=.5, areaRng='medium', maxDets=max_dets, lrp_type='oLRP')
+		stats['oLRP_large'] = _summarize(-1, iouThr=.5, areaRng='large', maxDets=max_dets,  lrp_type='oLRP')
+
 		return stats
 
 	def summarize(self):
