3a7987d1706a997c7ec9f53dee1cdbea49ecae9f
VietDunghacker <trungdunghoang92@gmail.com>
2022-05-22 12:41:34 +0900

    Update:

--- configs/general/efficientnet_bifpn_sepc_reppointsv2.py
+++ configs/general/efficientnet_bifpn_sepc_reppointsv2.py
@@ -6,6 +6,7 @@ model = dict(
 	backbone=dict(
 		type='EfficientNet',
 		arch='b7',
+		drop_path_rate=0.2,
 		out_indices=(3, 4, 5),
 		frozen_stages=0,
 		norm_cfg=dict(type='BN', requires_grad=True, eps=1e-3, momentum=0.01),
