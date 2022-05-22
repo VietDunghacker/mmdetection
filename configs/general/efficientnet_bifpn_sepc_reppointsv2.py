ef46c1e7d3d31b33cb6d50efd416ed6b401b86aa
VietDunghacker <trungdunghoang92@gmail.com>
2022-05-22 19:16:46 +0900

    update:

--- configs/general/efficientnet_bifpn_sepc_reppointsv2.py
+++ configs/general/efficientnet_bifpn_sepc_reppointsv2.py
@@ -175,15 +175,10 @@ data = dict(
 # optimizer
 optimizer = dict(
 	_delete_ = True,
-	type='AdamW',
-	lr=0.0001,
-	betas=(0.9, 0.999),
-	weight_decay=0.05,
-	paramwise_cfg=dict(
-		custom_keys={
-			'absolute_pos_embed': dict(decay_mult=0.),
-			'relative_position_bias_table': dict(decay_mult=0.),
-			'norm': dict(decay_mult=0.)}))
+	type='SGD',
+	lr=0.01,
+	momentum=0.937,
+	weight_decay=0.000036)
 optimizer_config = dict(grad_clip=None)
 log_config = dict(interval = 10)
 # learning policy
