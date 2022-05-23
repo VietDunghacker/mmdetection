a030f1bfbf7eb9d62f51988acd8f068a025fae1b
VietDunghacker <trungdunghoang92@gmail.com>
2022-05-23 18:40:51 +0900

    Update

--- configs/general/swin_bifpn_sepc_reppointsv2.py
+++ configs/general/swin_bifpn_sepc_reppointsv2.py
@@ -110,18 +110,11 @@ albu_train_transforms = [
 			dict(type='ToGray', p = 1.0)
 		],
 		p=0.1),
-	dict(
-		type='OneOf',
-		transforms=[
-			dict(type='MedianBlur', blur_limit=3, p=1.0),
-			dict(type='Blur', blur_limit=3, p=1.0),
-		],
-		p=0.1)
 ]
 
 train_pipeline = [
 	dict(type = 'FocusBoundingBox'),
-	#dict(type = 'RandomMaskFace', mask_face_prob=0.25),
+	#dict(type = 'RandomMaskFace', mask_face_prob=0.25),	
 	dict(
 		type = 'AutoAugment',
 		policies = [
@@ -130,11 +123,9 @@ train_pipeline = [
 				dict(type='Resize', img_scale=[(800, 800), (960, 960)], multiscale_mode='range', keep_ratio=True),
 			],
 			[
-				dict(type='RandomCrop', crop_type='relative_range', crop_size=(0.95, 0.95), allow_negative_crop = True),
 				dict(type='Resize', img_scale=[(640, 640), (960, 960)], multiscale_mode='range', keep_ratio=True),
 			],
 			[
-				dict(type='RandomCrop', crop_type='relative_range', crop_size=(0.95, 0.95), allow_negative_crop = True),
 				dict(type='Resize', img_scale=[(640, 640), (960, 960)], multiscale_mode='range', keep_ratio=True),
 			]
 		]
