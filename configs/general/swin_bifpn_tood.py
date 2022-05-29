bb862c214ed1a6af8d238cb499e4cfe71785758f
VietDunghacker <trungdunghoang92@gmail.com>
2022-05-29 18:23:44 +0900

    Update

--- configs/general/swin_bifpn_tood.py
+++ configs/general/swin_bifpn_tood.py
@@ -28,7 +28,7 @@ model = dict(
 		strides=[8, 16, 32],
 		num_layers=1,
 		weight_method='fast_attn',
-		norm_cfg=dict(type='BN', requires_grad=True),
+		norm_cfg=dict(type='GN', num_groups=32, requires_grad=True),
 		act_cfg='silu',
 		separable_conv=True,
 		epsilon=0.0001,
@@ -86,11 +86,11 @@ dataset_type = 'CocoDataset'
 data_root = 'data/coco/'
 img_norm_cfg = dict(mean=[123.675, 116.28, 103.53], std=[58.395, 57.12, 57.375], to_rgb=True)
 albu_train_transforms = [
-	dict(type='ShiftScaleRotate', shift_limit=0.1, scale_limit=0.1, rotate_limit=1, interpolation=1, p=0.5, border_mode = 0),
+	dict(type='ShiftScaleRotate', shift_limit=0.0, scale_limit=0.1, rotate_limit=1, interpolation=1, p=0.5, border_mode = 0),
 	dict(type='ColorJitter', brightness=0.2, contrast=0.2, saturation=0.2, hue=0.2),
 	dict(type='RGBShift', r_shift_limit=20, g_shift_limit=20, b_shift_limit=20),
 	dict(type='HueSaturationValue', hue_shift_limit=20, sat_shift_limit=30, val_shift_limit=20),
-	#dict(type='RandomRotate90'),
+	dict(type='RandomRotate90'),
 	dict(
 		type='OneOf',
 		transforms=[
@@ -108,29 +108,27 @@ albu_train_transforms = [
 ]
 
 train_pipeline = [
+	dict(type = 'FocusBoundingBox'),
+	#dict(type = 'RandomMaskFace', mask_face_prob=0.25),	
 	dict(
 		type = 'AutoAugment',
 		policies = [
 			[
-				dict(type='Mosaic', center_ratio_range=(0.9, 1.1), img_scale=(960, 960), pad_val=0.0),
+				dict(type='Mosaic', center_ratio_range=(0.95, 1.05), img_scale=(960, 960), pad_val=0.0),
 				dict(type='Resize', img_scale=[(800, 800), (960, 960)], multiscale_mode='range', keep_ratio=True),
 			],
 			[
-				dict(type='RandomCrop', crop_type='relative_range', crop_size=(0.9, 0.9), allow_negative_crop = True),
 				dict(type='Resize', img_scale=[(640, 640), (960, 960)], multiscale_mode='range', keep_ratio=True),
 			],
 			[
-				dict(type='RandomCrop', crop_type='relative_range', crop_size=(0.9, 0.9), allow_negative_crop = True),
 				dict(type='Resize', img_scale=[(640, 640), (960, 960)], multiscale_mode='range', keep_ratio=True),
 			]
 		]
 	),
 	dict(
 		type='CutOut',
-		n_holes=(5, 10),
-		cutout_shape=[(4, 4), (4, 8), (8, 4), (8, 8),
-					  (16, 8), (8, 16), (16, 16), (16, 32), (32, 16), (32, 32),
-					  (32, 48), (48, 32), (48, 48)]),
+		n_holes=(5, 25),
+		cutout_shape=[(4, 4), (4, 8), (8, 4), (8, 8), (16, 8), (8, 16), (16, 16), (16, 32), (32, 16), (32, 32)]),
 	dict(type='RandomFlip', flip_ratio=0.5),
 	dict(
 		type='Albu',
