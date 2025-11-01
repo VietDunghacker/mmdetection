5bca1fddd9af817cd84e7260c7c57d09b23b840a
VietDunghacker <trungdunghoang92@gmail.com>
2025-11-01 18:14:09 +0700

    Feature: Add ViT-Adapter

--- mmdet/models/backbones/vit_adapter.py
+++ mmdet/models/backbones/vit_adapter.py
@@ -45,17 +45,11 @@ class PatchEmbed(nn.Module):
         return x, H, W
 
 
-class Attention(nn.Module):
+class Attention(nn.MultiheadAttention):
     def __init__(self, dim, num_heads=8, qkv_bias=False,
                  attn_drop=0., proj_drop=0.):
-        super().__init__()
-        self.num_heads = num_heads
-        head_dim = dim // num_heads
-        self.scale = head_dim**-0.5
-
+        super().__init__(embed_dim=dim, num_heads=num_heads, drop_out=attn_drop)
         self.qkv = nn.Linear(dim, dim * 3, bias=qkv_bias)
-        self.attn_drop = nn.Dropout(attn_drop)
-        self.proj = nn.Linear(dim, dim)
         self.proj_drop = nn.Dropout(proj_drop)
 
     def forward(self, x, H, W):
@@ -63,12 +57,7 @@ class Attention(nn.Module):
         qkv = self.qkv(x).reshape(B, N, 3, self.num_heads, C // self.num_heads).permute(2, 0, 3, 1, 4)
         q, k, v = qkv.unbind(0)  # make torchscript happy (cannot use tensor as tuple)
 
-        attn = (q @ k.transpose(-2, -1)) * self.scale
-        attn = attn.softmax(dim=-1)
-        attn = self.attn_drop(attn)
-
-        x = (attn @ v).transpose(1, 2).reshape(B, N, C)
-        x = self.proj(x)
+        attn = super().forward(q, k, v, need_weights=False)
         x = self.proj_drop(x)
         return x
 
