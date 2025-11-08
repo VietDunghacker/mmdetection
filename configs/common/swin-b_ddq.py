e59016e4f69cdc55a7b33ee7289e5e4f52ca1b6a
VietDunghacker <trungdunghoang92@gmail.com>
2025-11-08 17:15:17 +0700

    Update config

--- configs/common/swin-b_ddq.py
+++ configs/common/swin-b_ddq.py
@@ -47,7 +47,7 @@ model = dict(
         embed_dims=128,
         depths=[2, 2, 18, 2],
         num_heads=[4, 8, 16, 32],
-        window_size=12,
+        window_size=7,
         mlp_ratio=4,
         qkv_bias=True,
         qk_scale=None,
@@ -57,7 +57,6 @@ model = dict(
         patch_norm=True,
         out_indices=(1, 2, 3),
         with_cp=True,
-        convert_weights=True,
         init_cfg=dict(type='Pretrained', checkpoint='https://github.com/SwinTransformer/storage/releases/download/v1.0.0/swin_base_patch4_window12_384_22k.pth')),
     neck=dict(
         type='ChannelMapper',
