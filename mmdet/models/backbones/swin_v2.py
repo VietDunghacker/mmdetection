9450572e94ea9787beef0fcef5c78de97552455b
VietDunghacker <trungdunghoang92@gmail.com>
2025-11-09 00:18:12 +0700

    SwinTransformer v2

--- mmdet/models/backbones/swin_v2.py
+++ mmdet/models/backbones/swin_v2.py
@@ -37,69 +37,6 @@ try:
 except ImportError:
     pass
 
-
-class Mlp(nn.Module):
-    """Multilayer perceptron used in Vision Transformers."""
-
-    def __init__(self,
-                 in_features,
-                 hidden_features=None,
-                 out_features=None,
-                 act_layer=nn.GELU,
-                 drop=0.):
-        super().__init__()
-        out_features = out_features or in_features
-        hidden_features = hidden_features or in_features
-        self.fc1 = nn.Linear(in_features, hidden_features)
-        self.act = act_layer()
-        self.fc2 = nn.Linear(hidden_features, out_features)
-        self.drop = nn.Dropout(drop)
-
-    def forward(self, x):
-        """Forward function."""
-        x = self.fc1(x)
-        x = self.act(x)
-        x = self.drop(x)
-        x = self.fc2(x)
-        x = self.drop(x)
-        return x
-
-
-def window_partition(x, window_size):
-    """
-    Args:
-        x: (B, H, W, C)
-        window_size (int): window size
-
-    Returns:
-        windows: (num_windows*B, window_size, window_size, C)
-    """
-    B, H, W, C = x.shape
-    x = x.view(B, H // window_size, window_size, W // window_size, window_size,
-               C)
-    windows = x.permute(0, 1, 3, 2, 4,
-                        5).contiguous().view(-1, window_size, window_size, C)
-    return windows
-
-
-def window_reverse(windows, window_size, H, W):
-    """
-    Args:
-        windows: (num_windows*B, window_size, window_size, C)
-        window_size (int): Window size
-        H (int): Height of image
-        W (int): Width of image
-
-    Returns:
-        x: (B, H, W, C)
-    """
-    B = int(windows.shape[0] / (H * W / window_size / window_size))
-    x = windows.view(B, H // window_size, W // window_size, window_size,
-                     window_size, -1)
-    x = x.permute(0, 1, 3, 2, 4, 5).contiguous().view(B, H, W, -1)
-    return x
-
-
 class WindowMSA(BaseModule):
     """Window based multi-head self attention (W-MSA) module with relative
     position bias. It supports both of shifted and non-shifted window.
@@ -272,8 +209,6 @@ class ShiftWindowMSA(BaseModule):
             right-bottom. If zero, act as regular window-msa. Defaults to 0.
         qkv_bias (bool, optional): If True, add a learnable bias to q, k, v.
             Default: True
-        qk_scale (float | None, optional): Override default qk scale of
-            head_dim ** -0.5 if set. Defaults: None.
         attn_drop_rate (float, optional): Dropout ratio of attention weight.
             Defaults: 0.
         proj_drop_rate (float, optional): Dropout ratio of output.
@@ -290,7 +225,6 @@ class ShiftWindowMSA(BaseModule):
                  window_size,
                  shift_size=0,
                  qkv_bias=True,
-                 qk_scale=None,
                  attn_drop_rate=0,
                  proj_drop_rate=0,
                  dropout_layer=dict(type='DropPath', drop_prob=0.),
@@ -307,7 +241,6 @@ class ShiftWindowMSA(BaseModule):
             num_heads=num_heads,
             window_size=to_2tuple(window_size),
             qkv_bias=qkv_bias,
-            qk_scale=qk_scale,
             attn_drop_rate=attn_drop_rate,
             proj_drop_rate=proj_drop_rate,
             pretrained_window_size=pretrained_window_size,
@@ -432,8 +365,6 @@ class SwinBlock(BaseModule):
         window_size (int, optional): The local window scale. Default: 7.
         shift (bool, optional): whether to shift window or not. Default False.
         qkv_bias (bool, optional): enable bias for qkv if True. Default: True.
-        qk_scale (float | None, optional): Override default qk scale of
-            head_dim ** -0.5 if set. Default: None.
         drop_rate (float, optional): Dropout rate. Default: 0.
         attn_drop_rate (float, optional): Attention dropout rate. Default: 0.
         drop_path_rate (float, optional): Stochastic depth rate. Default: 0.
@@ -455,7 +386,6 @@ class SwinBlock(BaseModule):
                  window_size=7,
                  shift=False,
                  qkv_bias=True,
-                 qk_scale=None,
                  drop_rate=0.,
                  attn_drop_rate=0.,
                  drop_path_rate=0.,
@@ -477,7 +407,6 @@ class SwinBlock(BaseModule):
             window_size=window_size,
             shift_size=window_size // 2 if shift else 0,
             qkv_bias=qkv_bias,
-            qk_scale=qk_scale,
             attn_drop_rate=attn_drop_rate,
             proj_drop_rate=drop_rate,
             dropout_layer=dict(type='DropPath', drop_prob=drop_path_rate),
@@ -527,8 +456,6 @@ class SwinBlockSequence(BaseModule):
         depth (int): The number of blocks in this stage.
         window_size (int, optional): The local window scale. Default: 7.
         qkv_bias (bool, optional): enable bias for qkv if True. Default: True.
-        qk_scale (float | None, optional): Override default qk scale of
-            head_dim ** -0.5 if set. Default: None.
         drop_rate (float, optional): Dropout rate. Default: 0.
         attn_drop_rate (float, optional): Attention dropout rate. Default: 0.
         drop_path_rate (float | list[float], optional): Stochastic depth
@@ -553,7 +480,6 @@ class SwinBlockSequence(BaseModule):
                  depth,
                  window_size=7,
                  qkv_bias=True,
-                 qk_scale=None,
                  drop_rate=0.,
                  attn_drop_rate=0.,
                  drop_path_rate=0.,
@@ -580,7 +506,6 @@ class SwinBlockSequence(BaseModule):
                 window_size=window_size,
                 shift=False if i % 2 == 0 else True,
                 qkv_bias=qkv_bias,
-                qk_scale=qk_scale,
                 drop_rate=drop_rate,
                 attn_drop_rate=attn_drop_rate,
                 drop_path_rate=drop_path_rates[i],
@@ -887,7 +812,6 @@ class SwinTransformerV2(BaseModule):
                 depth=depths[i],
                 window_size=window_size,
                 qkv_bias=qkv_bias,
-                qk_scale=qk_scale,
                 drop_rate=drop_rate,
                 attn_drop_rate=attn_drop_rate,
                 drop_path_rate=dpr[sum(depths[:i]):sum(depths[:i + 1])],
